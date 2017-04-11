99b8522d5286d966f0bfdf527e7cf934d0dede3c
doubleface <christophe@thiriot.xyz>
2017-04-11 09:34:46 +0200

    Added lint integration

--- config/csp-proof-sprite.js
+++ config/csp-proof-sprite.js
@@ -1,16 +1,16 @@
-var Sprite = require('svg-sprite-loader/lib/web/sprite');
-var svgOpening = '<svg id="svg-sprite-content" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"';
-var contentPlaceHolder = '{content}';
-var svgClosing = '</svg>';
-Sprite.spriteTemplate = function(){ return svgOpening +'><defs>' + contentPlaceHolder + '</defs>' + svgClosing; }
-var globalSprite = new Sprite();
+var Sprite = require('svg-sprite-loader/lib/web/sprite')
+var svgOpening = '<svg id="svg-sprite-content" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"'
+var contentPlaceHolder = '{content}'
+var svgClosing = '</svg>'
+Sprite.spriteTemplate = function () { return svgOpening + '><defs>' + contentPlaceHolder + '</defs>' + svgClosing }
+var globalSprite = new Sprite()
 
 if (document.body) {
-  globalSprite.elem = globalSprite.render(document.body);
+  globalSprite.elem = globalSprite.render(document.body)
 } else {
   document.addEventListener('DOMContentLoaded', function () {
-    globalSprite.elem = globalSprite.render(document.body);
-  }, false);
+    globalSprite.elem = globalSprite.render(document.body)
+  }, false)
 }
 
-module.exports = globalSprite;
+module.exports = globalSprite
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 PATH="./node_modules/.bin:$PATH"
 
-./node_modules/.bin/standard "**/*.js" "**/*.jsx"
+./node_modules/.bin/standard "**/*.js" "client/**/*.jsx"
