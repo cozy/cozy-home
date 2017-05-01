9f48683aec6130086dd9b0706d31f49998badfd5
Grégory Le Garec <gregory.legarec@gmail.com>
2017-05-02 00:39:03 +0200

    feat: display konnector name :pencil:

--- app/containers/IntentService.jsx
+++ app/containers/IntentService.jsx
@@ -14,15 +14,36 @@ export default class IntentService extends Component {
       isFetching: true
     }
 
-    // Maybe the logic getting the intent from location.search should be
+    // Maybe the logic about getting the intent from location.search should be
     // encapsulated in cozy.client.createService
     const intent = window.location.search.split('=')[1]
 
     this.store.createIntentService(intent, window)
-      .then(service => this.setState({
-        isFetching: false,
-        service: service
-      }))
+      .then(service => {
+        this.setState({
+          service: service
+        })
+
+        const data = service.getData()
+
+        if (!data || !data.slug) {
+          throw new Error('Unexpected data from intent')
+        }
+
+        return this.store.fetchKonnectorInfos(data.slug)
+      })
+      .then(konnector => {
+        if (!konnector) {
+          throw new Error(`Unknown konnector`)
+        }
+
+        this.setState({
+          isFetching: false,
+          konnector: konnector
+        })
+
+        return konnector
+      })
       .catch(error => {
         this.setState({
           isFetching: false,
@@ -32,16 +53,18 @@ export default class IntentService extends Component {
   }
 
   render () {
-    const { isFetching, error } = this.state
+    const { isFetching, error, konnector } = this.state
     const { t } = this.context
     return (
       <div class='coz-service'>
         { isFetching && <Loading /> }
         { error && <div class='coz-error coz-service-error'>
           <h1>{t('intent.service.error')}</h1>
-          <p>{t('intent.service.error.cause', {error: error})}</p>
+          <p>{t('intent.service.error.cause', {error: error.message})}</p>
         </div>}
-      </div>
-    )
+        { !isFetching && !error && konnector &&
+          <h1>{konnector.name}</h1>
+        }
+      </div>)
   }
 }
