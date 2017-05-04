af74ec38b10c1d20114c50ff13515d9f3cb647f6
CPatchane <code@patchane.com>
2017-05-04 15:20:32 +0200

    style: better use cases styles :lipstick:

--- app/components/ConnectorItem.jsx
+++ app/components/ConnectorItem.jsx
@@ -3,7 +3,7 @@ import { h } from 'preact'
 import { Link, withRouter } from 'react-router'
 import { translate } from '../plugins/preact-polyglot'
 
-const ConnectorItem = ({ title, subtitle, connected, slug, iconName, backgroundCSS, enableDefaultIcon = false, router }) => (
+const ConnectorItem = ({ title, subtitle, connected, slug, iconName, backgroundCSS, enableDefaultIcon = false, isUseCase, router }) => (
   <Link class='item-wrapper' to={`${router.location.pathname}/${slug}`}>
     <header class='item-header' style={{background: backgroundCSS}}>
       {iconName &&
@@ -12,7 +12,7 @@ const ConnectorItem = ({ title, subtitle, connected, slug, iconName, backgroundC
         </svg>
       }
     </header>
-    <p class='item-title'>{title}</p>
+    <p className={isUseCase ? 'use-case-title' : 'item-title'}>{title}</p>
     {subtitle && <p class='item-subtitle'>{subtitle}</p>}
     {connected &&
       <svg class='item-connected'>
