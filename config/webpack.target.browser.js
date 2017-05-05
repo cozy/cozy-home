e1b0293ea0c3a13416d25cb25ad579617e043942
CPatchane <code@patchane.com>
2017-05-05 13:53:43 +0200

    chore: update/get webpack global configuration more consistant :wrench:

---
+++ config/webpack.target.browser.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, '../src/index'),
+    services: path.resolve(__dirname, '../src/services')
+  },
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: '[name].js'
+  },
+  externals: {
+    'cozy-client-js': 'cozy'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __TARGET__: JSON.stringify('browser')
+    })
+  ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,158 +1,21 @@
 'use strict'
 
-const path = require('path')
-
-const autoprefixer = require('autoprefixer')
-
-const CopyPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const pkg = require(path.resolve(__dirname, 'package.json'))
-
 const merge = require('webpack-merge')
-const { production, extractor } = require('./config/webpack.vars')
-// use the `NODE_ENV` env to switch from dev to production build
-const optimize = process.env.NODE_ENV === production
+const { production, target } = require('./config/webpack.vars')
 
-/**
- * Loaders used by webpack
- *
- * - CSS and images files from `vendor` are excluded
- * - stylesheets are optimized via cssnano, minus svgo and autoprefixer that are
- * customized via PostCSS
- * - images are cache-busted in production build
- */
-const cssOptions = optimize ? 'css?-svgo&-autoprefixer&-mergeRules' : 'css'
-const imgPath = 'img/' + '[name]' + (optimize ? '.[hash]' : '') + '.[ext]'
-
-let loaders = [
-  {
-    test: /\.jsx?$/,
-    exclude: /(node_modules|cozy-(bar|client-js))/,
-    loader: 'babel-loader',
-    query: {
-      presets: [
-        ['env', {
-          'targets': {
-            'chrome': 42,
-            'browsers': ['last 2 versions']
-          },
-          'useBuiltIns': true
-        }]
-      ],
-      plugins: [['transform-react-jsx', { 'pragma': 'h' }]]
-    }
-  },
-  {
-    test: /\.json$/,
-    loader: 'json'
-  },
-  {
-    test: /\.styl$/,
-    loader: extractor.extract('style', cssOptions + '!postcss-loader!stylus')
-  },
-  {
-    test: /\.svg$/,
-    include: /(sprites|icons)/,
-    loader: 'svg-sprite?' + JSON.stringify({
-      name: '[name]_[hash]',
-      spriteModule: path.join(__dirname, 'config/csp-proof-sprite')
-    })
-  },
-  {
-    test: /\.(png|gif|jpe?g|svg)$/i,
-    exclude: /(vendor|sprites|icons)/,
-    loader: 'file?name=' + imgPath
-  }
-]
-
-/**
- * Configure Webpack's plugins to tweaks outputs:
- *
- * all builds:
- * - ExtractTextPlugin: output CSS to file instead of inlining it
- * - CopyPlugin: copy assets to public dir
- *
- * prod build:
- * - AssetsPlugin: paths to cache-busted's assets to read them from server
- * - DedupePlugin
- * - OccurenceOrderPlugin
- * - UglifyJsPlugin
- * - DefinePlugin: disable webpack env dev vars
- *
- * dev build:
- * - BrowserSyncPlugin: make hot reload via browsersync exposed at
- *   http://localhost:3000, proxified to the server app port
- */
-let plugins = [
-  extractor,
-  new CopyPlugin([
-    { from: 'vendor/assets', ignore: ['.gitkeep'] }
-  ]),
-  new HtmlWebpackPlugin({
-    template: 'app/index.ejs',
-    title: pkg.name,
-    inject: false,
-    excludeChunks: ['services'],
-    minify: {
-      collapseWhitespace: true
-    }
-  }),
-  new HtmlWebpackPlugin({
-    template: 'app/services.ejs',
-    title: `${pkg.name} services`,
-    filename: 'services/index.html',
-    inject: false,
-    chunks: ['services'],
-    minify: {
-      collapseWhitespace: true
-    }
-  })
-]
-
-/**
- * Webpack config
- *
- * - output to `public` dir
- * - cache-bust assets when build for production
- */
-
-module.exports = merge(require('./config/webpack.config.base.js'), {
-  entry: {
-    app: './app',
-    services: './app/services.jsx'
-  },
-  output: {
-    path: path.resolve('build'),
-    filename: optimize ? '[name].[hash].js' : '[name].js'
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx', '.json'],
-    // so that npm linked libs can be found
-    fallback: path.join(__dirname, 'node_modules'),
-    alias: {
-      'react': 'preact-compat',
-      'react-dom': 'preact-compat'
-    }
-  },
-  resolveLoader: { fallback: path.join(__dirname, 'node_modules') },
-  debug: !optimize,
-  devtool: 'source-map',
-  module: {
-    loaders: loaders
-  },
-  plugins: plugins,
-  postcss: function () {
-    return [
-      autoprefixer({
-        browsers: [
-          'last 2 versions'
-        ]
-      })
-    ]
-  },
-  stylus: {
-    use: [require('cozy-ui/stylus')()]
-  }
-},
-  require(production ? './config/webpack.config.prod' : './config/webpack.config.dev')
+const common = merge(
+  require('./config/webpack.config.base'),
+  // require('./config/webpack.config.disable-contexts'),
+  require('./config/webpack.config.preact'),
+  require('./config/webpack.config.cozy-ui'),
+  require('./config/webpack.config.pictures'),
+  require('./config/webpack.config.vendors'),
+  require('./config/webpack.config.manifest'),
+  require(`./config/webpack.target.${target}`)
 )
+
+if (production) {
+  module.exports = merge(common, require('./config/webpack.config.prod'))
+} else {
+  module.exports = merge(common, require('./config/webpack.config.dev'))
+}
