820f632a211d7b6a3cbba95a30a2a88a57185520
CPatchane <code@patchane.com>
2017-05-05 12:03:18 +0200

    chore: update package.json (from template) and npm scripts :wrench:

--- scripts/tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-PATH="./node_modules/.bin:$PATH"
-
-mocha \
-    --reporter spec \
-    --colors \
-    --globals clearImmediate,setImmediate \
-    --compilers coffee:coffee-script/register \
-    tests
