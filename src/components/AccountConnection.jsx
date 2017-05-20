a629499e1c9a1d54ff1575ae308437942f7f1be7
Grégory Le Garec <gregory.legarec@gmail.com>
2017-05-21 01:05:07 +0200

    feat: CSS Modules and Connect button in  AccountLoginForm :art:

--- src/components/AccountConnection.jsx
+++ src/components/AccountConnection.jsx
@@ -1,18 +1,14 @@
 import React, { Component } from 'react'
 import statefulForm from '../lib/statefulForm'
 
-import AccountLoginForm from './AccountLoginForm'
 import DataItem from './DataItem'
 import ReactMarkdown from 'react-markdown'
 
 class AccountConnection extends Component {
   render () {
-    const { t, connector, fields, error, submitting } = this.props
-    let { dirty } = this.props
-    const { name, description } = connector
+    const { t, connector } = this.props
+    const { description } = connector
 
-    // If there is no field displayed, the form is dirty by default.
-    dirty = dirty || Object.values(fields).every(field => field.type === 'hidden' || field.advanced)
     return (
       <div className='account-connection'>
         <div className='account-description'>
@@ -35,29 +31,6 @@ class AccountConnection extends Component {
             )}
           </ul>
           <p>{` ${connector.name} ${t('dataType disclaimer')} `}</p>
-        </div>
-        <div className='account-login'>
-          <h3>{t('account config title', {name: name})}</h3>
-          <div className={'account-form' + (error ? ' error' : '')}>
-            <AccountLoginForm
-              t={t}
-              fields={fields}
-            />
-            <div className='account-form-controls'>
-              <button
-                disabled={!dirty && !connector.oauth}
-                aria-busy={submitting ? 'true' : 'false'}
-                onClick={connector.oauth
-                  ? () => this.props.oauth(connector.slug)
-                  : () => this.props.submit()}
-              >
-                {t('account config button')}
-              </button>
-              {error === 'bad credentials' &&
-                <p className='errors'>{t('account config bad credentials')}</p>
-              }
-            </div>
-          </div>
         </div>
       </div>
     )
--- src/components/AccountLoginForm.jsx
+++ src/components/AccountLoginForm.jsx
@@ -1,9 +1,17 @@
+import styles from '../styles/accountLoginForm'
+
 import React from 'react'
 
 import Field, { PasswordField, DropdownField, CheckboxField } from './Field'
 
-const AccountLoginForm = ({ t, fields }) => (
-  <div className='account-form-login'>
+const AccountLoginForm = ({ t, connector, customView, fields, error, dirty, submitting, onSubmit }) => (
+  <div className={styles['account-form-login']}>
+    {customView &&
+      <div className='coz-custom-view'
+        dangerouslySetInnerHTML={{
+          __html: customView.replace(/<%t (.*) %>/gi, (match, $1) => t($1))
+        }} />
+    }
     {Object.keys(fields)
       .filter(name => !fields[name].advanced)
       .map(name => {
@@ -21,6 +29,19 @@ const AccountLoginForm = ({ t, fields }) => (
         return <Field label={t(name)} {...fields[name]} />
       }
     )}
+    <div>
+      <button
+        className='coz-button--primary'
+        disabled={!dirty && !connector.oauth}
+        aria-busy={submitting ? 'true' : 'false'}
+        onClick={onSubmit}
+      >
+        {t('account.connection.login.submit')}
+      </button>
+      {error === 'bad credentials' &&
+        <p className='errors'>{t('account.connection.login.error.bad_credentials')}</p>
+      }
+    </div>
   </div>
 )
 
