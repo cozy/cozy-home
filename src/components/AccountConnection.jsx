4fcaa138c993ea5c199e43386700ae3bf49831ee
Grégory Le Garec <gregory.legarec@gmail.com>
2017-05-23 00:21:42 +0200

    feat: add konnector icon in account login form :lipstick:

--- src/components/AccountConnection.jsx
+++ src/components/AccountConnection.jsx
@@ -8,11 +8,43 @@ import DataItem from './DataItem'
 import ReactMarkdown from 'react-markdown'
 
 class AccountConnection extends Component {
+  constructor (props, context) {
+    super(props, context)
+
+    this.state = {
+      submitting: false
+    }
+
+    this.submit = () => {
+      this.setState({
+        submitting: true
+      })
+
+      this.props.submit()
+        .catch(error => this.setState({submitting: false, error: error.message}))
+    }
+  }
+
+  // TODO: use a better helper
+  getIcon (konnector) {
+    try {
+      return require(`../assets/icons/color/${konnector.slug}.svg`)
+    } catch (error) {
+      console.warn(error.message)
+      return require('../assets/icons/color/default.svg')
+    }
+  }
+
   render () {
     const { t, connector, fields } = this.props
     const { customView, description } = connector
     return (
       <div className={styles['col-account-connection']}>
+        <div className={styles['col-account-connection-header']}>
+          <svg>
+            <use xlinkHref={this.getIcon(connector)} />
+          </svg>
+        </div>
         <div className={styles['col-account-connection-content']}>
           <div className={styles['col-account-connection-form']}>
             <h3>{t('account.connection.title', { name: connector.name })}</h3>
