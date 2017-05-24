32b604996c0fcbd53c41ee896047f49040aece75
Grégory Le Garec <gregory.legarec@gmail.com>
2017-05-24 18:40:37 +0200

    feat: stylize Account Management form :lipstick:

--- src/components/AccountManagement.jsx
+++ src/components/AccountManagement.jsx
@@ -1,5 +1,8 @@
+import styles from '../styles/accountManagement'
+
 import React from 'react'
 import statefulForm from '../lib/statefulForm'
+import classNames from 'classnames'
 
 import AccountConfigForm from './AccountConfigForm'
 
@@ -10,33 +13,35 @@ const AccountManagement = (props) => {
   const isLoginFilled = !!props.values.login || !!props.values.access_token
   return (
     <div>
-      <div className='account-management'>
-        <div className='account-config'>
-          <AccountConfigForm {...props} />
-          {isLoginFilled
-          ? <div>
-            <h3>{t('disconnect')}</h3>
-            <p>
-              {t('disconnect desc')}
-            </p>
-            <button className='danger' disabled={deleting} onClick={() => deleteAccount(accounts[selectedAccount])}>
-              {t('disconnect button')}
-            </button>
-          </div>
-          : <div>
-            <button className='danger' disabled={deleting} onClick={() => deleteAccount(accounts[selectedAccount])}>
-              {t('delete button')}
-            </button>
-          </div>}
-
+      <AccountConfigForm {...props} />
+      { isLoginFilled &&
+        <div>
+          <h3>{t('disconnect')}</h3>
+          <p>
+            {t('disconnect desc')}
+          </p>
+          <button className='danger' disabled={deleting} onClick={() => deleteAccount(accounts[selectedAccount])}>
+            {t('disconnect button')}
+          </button>
         </div>
-      </div>
-      <div className='account-management-controls'>
-        <button className='cancel' onClick={cancel}>
+      }
+      { !isLoginFilled &&
+        <div>
+          <button className='danger' disabled={deleting} onClick={() => deleteAccount(accounts[selectedAccount])}>
+            {t('delete button')}
+          </button>
+        </div>
+      }
+      <div className={styles['account-management-controls']}>
+        <button
+          className={classNames('coz-btn', 'coz-btn--secondary', styles['coz-btn'])}
+          onClick={cancel}
+        >
           {t('account cancel button')}
         </button>
         <button
-          disabled={!dirty}
+          className={classNames('coz-btn', 'coz-btn--regular', styles['coz-btn'])}
+          disabled={!dirty || submitting}
           aria-busy={submitting ? 'true' : 'false'}
           onClick={submit}
         >
