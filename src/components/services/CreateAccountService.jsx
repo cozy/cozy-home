8397245adbbbd2de7cc8b5d0146656ff969a5c70
doubleface <christophe@thiriot.xyz>
2017-05-25 18:13:05 +0200

    feat added OAuth in interapp service

--- src/components/services/CreateAccountService.jsx
+++ src/components/services/CreateAccountService.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import AccountConnection from '../AccountConnection'
 
 const CreateAccountService = (props) => {
-  const { konnector, onSubmit } = props
+  const { konnector, onSubmit, onOAuth } = props
   return (
     <div className='coz-service-content'>
       <AccountConnection
         connector={konnector}
         fields={konnector.fields}
         onSubmit={onSubmit}
+        onOAuth={onOAuth}
         {...props}
         />
     </div>
--- src/containers/ConnectorManagement.jsx
+++ src/containers/ConnectorManagement.jsx
@@ -5,6 +5,7 @@ import ModalContent from 'cozy-ui/react/Modal/Content'
 import AccountConnection from '../components/AccountConnection'
 import AccountManagement from '../components/AccountManagement'
 import Notifier from '../components/Notifier'
+import {popupCenter} from '../lib/popup'
 
 import { ACCOUNT_ERRORS } from '../lib/accounts'
 
@@ -15,36 +16,6 @@ let AUTHORIZED_DATATYPE = [
   'refund', 'sleepTime', 'courseMaterial', 'temperature', 'tweet'
 ]
 let isValidType = (type) => AUTHORIZED_DATATYPE.indexOf(type) !== -1
-
-// customized function to center a popup window
-// source https://stackoverflow.com/a/16861050
-function popupCenter (url, title, w, h) {
-  /* global screen */
-  // Fixes dual-screen position
-  //                      Most browsers      Firefox
-  var dualScreenLeft = window.screenLeft || screen.left
-  var dualScreenTop = window.screenTop || screen.top
-
-  var width = window.innerWidth
-    ? window.innerWidth
-    : document.documentElement.clientWidth
-      ? document.documentElement.clientWidth : screen.width
-  var height = window.innerHeight
-    ? window.innerHeight
-    : document.documentElement.clientHeight
-      ? document.documentElement.clientHeight : screen.height
-
-  var left = ((width / 2) - (w / 2)) + dualScreenLeft
-  var top = ((height / 2) - (h / 2)) + dualScreenTop
-  var newWindow = window.open('', title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left)
-  newWindow.location.href = url
-
-  // Puts focus on the newWindow
-  if (window.focus) {
-    newWindow.focus()
-  }
-  return newWindow
-}
 
 export default class ConnectorManagement extends Component {
   constructor (props, context) {
