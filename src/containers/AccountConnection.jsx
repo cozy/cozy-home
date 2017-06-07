79dca77e983a2d5292961e9509ddfbac809ea18c
CPatchane <code@patchane.com>
2017-06-07 09:49:32 +0200

    fix: remove now unused connector customView :fire:

--- src/containers/AccountConnection.jsx
+++ src/containers/AccountConnection.jsx
@@ -194,7 +194,7 @@ class AccountConnection extends Component {
   render () {
     const { t, existingAccount, connector, fields } = this.props
     const { submitting, deleting, error, credentialsError } = this.state
-    const { customView, description } = connector
+    const { description } = connector
     const securityIcon = require('../assets/icons/color/icon-cloud-lock.svg')
     return (
       <div className={styles['col-account-connection']}>
@@ -232,7 +232,6 @@ class AccountConnection extends Component {
               t={t}
               connectorSlug={connector.slug}
               isOAuth={connector.oauth}
-              customView={customView}
               fields={fields}
               submitting={submitting}
               deleting={deleting}
