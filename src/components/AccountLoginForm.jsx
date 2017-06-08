73f5698fbe269a586e444547fabf5dd9e9865174
CPatchane <code@patchane.com>
2017-06-08 12:39:43 +0200

    feat: add form field description :sparkles:

--- src/components/AccountLoginForm.jsx
+++ src/components/AccountLoginForm.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import classNames from 'classnames'
 import statefulForm from '../lib/statefulForm'
 import Field, { PasswordField, DropdownField, CheckboxField } from './Field'
+import ReactMarkdownWrapper from './ReactMarkdownWrapper'
 
 const AccountLoginForm = ({ t, isOAuth, fields, error, dirty, submitting, forceEnabled, deleting, values, submit, onDelete, onCancel, connectorSlug }) => {
   const isUpdate = Object.keys(values).length > 0
@@ -14,32 +15,44 @@ const AccountLoginForm = ({ t, isOAuth, fields, error, dirty, submitting, forceE
         .filter(name => !fields[name].advanced)
         .map(name => {
           const inputName = `${name}_${connectorSlug}`
-          if (fields[name].type === 'password') {
-            return <PasswordField
-              label={t(`account.form.label.${name}`)}
-              name={inputName}
-              placeholder={t('account.form.placeholder.password')}
-              invalid={!!error}
-              noAutoFill
-              {...fields[name]} />
+          const description = fields[name].description
+            ? <ReactMarkdownWrapper source={t(`connector.${connectorSlug}.description.field.${name}`)} />
+            : ''
+          switch (fields[name].type) {
+            case 'password':
+              return <div>
+                {description}
+                <PasswordField
+                  label={t(`account.form.label.${name}`)}
+                  name={inputName}
+                  placeholder={t('account.form.placeholder.password')}
+                  invalid={!!error}
+                  noAutoFill
+                  {...fields[name]} />
+              </div>
+            case 'dropdown':
+              return <div>
+                {description}
+                <DropdownField label={t(`account.form.label.${name}`)} {...fields[name]} />
+              </div>
+            case 'checkbox':
+              return <div>
+                {description}
+                <CheckboxField label={t(`account.form.label.${name}`)} {...fields[name]} />
+              </div>
+            default:
+              const readOnly = name === 'login' && isUpdate
+              return <div>
+                {description}
+                <Field
+                  label={t(`account.form.label.${name}`)}
+                  name={inputName}
+                  readOnly={readOnly}
+                  invalid={!!error}
+                  noAutoFill
+                  {...fields[name]} />
+              </div>
           }
-          if (fields[name].type === 'dropdown') {
-            return <DropdownField label={t(`account.form.label.${name}`)} {...fields[name]} />
-          }
-
-          if (fields[name].type === 'checkbox') {
-            return <CheckboxField label={t(`account.form.label.${name}`)} {...fields[name]} />
-          }
-
-          const readOnly = (name === 'login' || name === 'email') && isUpdate
-
-          return <Field
-            label={t(`account.form.label.${name}`)}
-            name={inputName}
-            readOnly={readOnly}
-            invalid={!!error}
-            noAutoFill
-            {...fields[name]} />
         }
       )}
       {isUpdate &&
