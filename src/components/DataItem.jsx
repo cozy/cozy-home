3b151f040cd9f78fda51face82619bcbfbdb47d2
CPatchane <code@patchane.com>
2017-06-28 15:35:41 +0200

    test: add DataItem tests according to datatypes.json config :white_check_mark:

--- src/components/DataItem.jsx
+++ src/components/DataItem.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/dataItem'
 import React from 'react'
 import { translate } from '../plugins/i18n'
 
-const DataItem = ({ t, dataType, hex }) => (
+export const DataItem = ({ t, dataType, hex }) => (
   <li className={styles['col-data-item']}>
     <svg className={styles['col-data-item-icon']} style={{color: hex}}>
       <use xlinkHref={require('../assets/sprites/icon-' + dataType + '.svg')} />
---
+++ test/components/DataItem.spec.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/* eslint-env jest */
+
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { tMock } from '../jestLib/I18n'
+import { DataItem } from '../../src/components/DataItem'
+
+import datatypesToTest from '../../src/config/datatypes'
+
+const hexColorMock = '#297EF2'
+
+describe('DataItem component', () => {
+  beforeEach(() => {
+    jest.resetModules()
+  })
+
+  for (const dataType of datatypesToTest) {
+    it(`should display correctly the ${dataType} data type`, () => {
+      const component = shallow(
+        <DataItem t={tMock} dataType={dataType} hex={hexColorMock} />
+      ).node
+      expect(component).toMatchSnapshot()
+    })
+  }
+})
