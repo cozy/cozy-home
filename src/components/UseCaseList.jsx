4b9a358bca349c65c7b7eb0dbe5865ab8a74b2f3
Grégory Le Garec <gregory.legarec@gmail.com>
2017-07-04 18:18:15 +0200

    refacto: separate UseCaseItem from KonnectorItem :art:

--- src/components/UseCaseList.jsx
+++ src/components/UseCaseList.jsx
@@ -1,30 +1,13 @@
 import React from 'react'
 import { translate } from 'cozy-ui/react/I18n'
-import KonnectorItem from './KonnectorItem'
-
-// Fallback to get the item background image and avoid error if not found
-const getItemBackground = (item, context) => {
-  let background = 'rgb(0, 130, 230)'
-  if (item.figure && context) {
-    try {
-      let img = require(`../contexts/${context}/assets/img/${item.figure}`)
-      background = `center/100% url(${img})`
-    } catch (e) {
-      background = 'rgb(0, 130, 230)'
-    }
-  }
-  return background
-}
+import UseCaseItem from './UseCaseItem'
 
 const UseCaseList = ({ t, useCases, context }) => (
   <div className='use-cases-list'>
-    {useCases.map(u =>
-      <KonnectorItem
-        title={t(`use-case ${u.slug} title`)}
-        slug={u.slug}
-        enableDefaultIcon={false}
-        backgroundCSS={getItemBackground(u, context)}
-        isUseCase
+    {useCases.map(useCase =>
+      <UseCaseItem
+        useCase={useCase}
+        context={context}
       />
     )}
   </div>
