f931af9f2bc7c06fbcc2911958f637d6e33a158f
Olivier Gendrin — Faire Front <olivier@fairefront.fr>
2017-07-05 21:10:38 +0200

    fix: last tweaks

--- src/components/KonnectorSync.jsx
+++ src/components/KonnectorSync.jsx
@@ -1,6 +1,7 @@
 import styles from '../styles/KonnectorSync'
 
 import React from 'react'
+import classNames from 'classnames'
 import { translate } from 'cozy-ui/react/I18n'
 
 import DescriptionContent from './DescriptionContent'
@@ -12,15 +13,16 @@ export const KonnectorSync = ({ t, f, date, editing, submitting, onForceConnecti
         title={t('account.message.synced.title')}
         messages={[t('account.message.synced.last_sync', { date: f(date, t('account.message.synced.date_format')) })]}
       /> }
-      { !editing && <div className={styles['account-forceConnection']}>
+      <div className={styles['account-forceConnection']}>
         <button
-          className={submitting ? styles['submitting'] : ''}
+          className={submitting ? classNames('coz-btn', styles['submitting']) : classNames('coz-btn') }
           disabled={submitting}
+          aria-busy={submitting}
           onClick={onForceConnection}
         >
           {t('account.forceConnection')}
         </button>
-      </div> }
+      </div>
     </div>
   )
 }
