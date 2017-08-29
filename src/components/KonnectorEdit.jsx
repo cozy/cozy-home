c78a506f2433096911dc3e3d3095a968f045db5d
m4dz <code@m4dz.net>
2017-08-29 12:04:23 +0200

    feat: add tabs for edition view

--- src/components/KonnectorEdit.jsx
+++ src/components/KonnectorEdit.jsx
@@ -2,10 +2,42 @@ import React from 'react'
 import { translate } from 'cozy-ui/react/I18n'
 import styles from '../styles/accountConnection'
 
+import { Tab, Tabs, TabList, TabPanels, TabPanel } from 'cozy-ui/react/Tabs'
+
 export const KonnectorEdit = ({ t }) => {
   return (
     <div className={styles['col-account-connection-content']}>
-      EDIT
+      <Tabs initialActiveTab='sync'>
+
+        <TabList>
+          <Tab name='sync'>
+            {t('account.config.tabs.sync')}
+          </Tab>
+          <Tab name='account'>
+            {t('account.config.tabs.account')}
+          </Tab>
+          <Tab name='data'>
+            {t('account.config.tabs.data')}
+          </Tab>
+        </TabList>
+
+        <TabPanels>
+
+          <TabPanel name='sync'>
+            SYNCHRONIZATION
+          </TabPanel>
+
+          <TabPanel name='account'>
+            ACCOUNT
+          </TabPanel>
+
+          <TabPanel name='data'>
+            DATA
+          </TabPanel>
+
+        </TabPanels>
+
+      </Tabs>
     </div>
   )
 }
