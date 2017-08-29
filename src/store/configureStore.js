ec76f002fa7ca6bfd231f25a3e9a3adfcf4fff43
Grégory Le Garec <gregory.legarec@gmail.com>
2017-08-29 17:16:23 +0200

    chore: add redux-logger :heavy_plus_sign:

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,17 @@
-import { createStore } from 'redux'
+/* global __DEVTOOLS__ */
+import { compose, createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
 import CollectStore from '../lib/CollectStore'
 import getReducers from '../reducers'
 
 const configureStore = (initKonnectors, initFolders, context) => {
-  const reduxStore = createStore(getReducers())
+  // Enable Redux dev tools
+  const composeEnhancers = (__DEVTOOLS__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+
+  const reduxStore = createStore(
+    getReducers(),
+    composeEnhancers(applyMiddleware.apply(this, [createLogger()]))
+  )
 
   return Object.assign(new CollectStore(initKonnectors, initFolders, context), reduxStore)
 }
