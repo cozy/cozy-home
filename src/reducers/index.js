98ec30e9e3a2e9cd821a9c815898a3c53db36e87
Grégory Le Garec <gregory.legarec@gmail.com>
2017-09-18 23:47:26 +0200

    feat: add getConnectionsQueue in main reducer :pencil:

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,18 @@
 import { combineReducers } from 'redux'
 
 import { reducer } from 'redux-cozy-client'
-import connections from '../ducks/connections'
 import registry from '../ducks/registry'
+import connections, {
+  getQueue as getConnectionsQueue
+} from '../ducks/connections'
 
 export default () => combineReducers({
   connections,
   cozy: reducer,
   registry
 })
+
+// selectors
+export const getQueue = (state) => {
+  return getConnectionsQueue(state.connections, state.registry.konnectors)
+}
