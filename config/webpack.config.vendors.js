bfda2c3bfb38c6ec6d691a8b459cdcca677cbe6b
CPatchane <code@patchane.com>
2017-09-26 17:04:31 +0200

    fix: add publish script in build branch :construction:

--- config/webpack.config.vendors.js
+++ config/webpack.config.vendors.js
@@ -6,7 +6,8 @@ module.exports = {
   plugins: [
     new CopyPlugin([
       { from: 'vendor/assets', ignore: ['.gitkeep'] },
-      { from: 'manifest.webapp' }
+      { from: 'manifest.webapp' },
+      { from: 'scripts/publish_registry.sh' }
     ])
   ]
 }
