ab1ee23d7da563e86496eadb54f791ab21fa247c
Grégory Le Garec <gregory.legarec@gmail.com>
2017-11-08 17:49:55 +0100

    feat: update Accounts duck :duck:📝

--- src/ducks/accounts/index.js
+++ src/ducks/accounts/index.js
@@ -1,6 +1,39 @@
-import { fetchCollection, fetchDocument } from 'redux-cozy-client'
+import {
+  createDocument,
+  fetchCollection,
+  fetchDocument
+} from 'redux-cozy-client'
 
 export const DOCTYPE = 'io.cozy.accounts'
+const accountCollectionKey = 'accounts'
 
-export const fetchAccounts = () => fetchCollection('accounts', DOCTYPE)
+export const createAccount = (auth, name) =>
+  createDocument(DOCTYPE, buildAccount(auth, name), {
+    updateCollections: [accountCollectionKey]
+  })
+
+export const fetchAccounts = () =>
+  fetchCollection(accountCollectionKey, DOCTYPE)
+
 export const fetchAccount = id => fetchDocument(DOCTYPE, id)
+
+// Factory helpers
+
+export const buildAccount = (auth, name) => {
+  const doc = { type: DOCTYPE, auth }
+
+  if (name) {
+    doc.name = name
+  }
+
+  return doc
+}
+
+// selectors
+export const getAccount = (state, id) => {
+  if (!state.documents || !state.documents[DOCTYPE]) {
+    return null
+  }
+
+  return state.documents[DOCTYPE][id]
+}
