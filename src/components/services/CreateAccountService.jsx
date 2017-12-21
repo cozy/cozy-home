a71238dde068cc53d469c6cd760f04e086d234b0
Marius <marius@cozycloud.cc>
2017-12-21 18:07:48 +0100

    feat: restore existingAccoun

--- src/components/services/CreateAccountService.jsx
+++ src/components/services/CreateAccountService.jsx
@@ -1,18 +1,46 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { getKonnectorConnectedAccount } from '../../ducks/connections'
+import {
+  getKonnectorConnectedAccount,
+  getTriggerByKonnector
+} from '../../reducers'
 
 import AccountConnection from '../../containers/AccountConnection'
 
 const CreateAccountService = props => {
-  const { existingAccount, konnector } = props
+  const { existingAccount, konnector, trigger, t } = props
+  console.log(props)
+  const values =
+    (existingAccount && Object.assign({}, existingAccount.auth)) || {}
+  // Split the actual folderPath account to get namePath & folderPath values
+  if (existingAccount && values.folderPath) {
+    values.folderPath = existingAccount.auth.folderPath.substring(
+      0,
+      existingAccount.auth.folderPath.lastIndexOf('/')
+    )
+    values.namePath = existingAccount.auth.folderPath.substring(
+      existingAccount.auth.folderPath.lastIndexOf('/') + 1,
+      existingAccount.auth.folderPath.length
+    )
+  } else if (
+    (existingAccount === null &&
+      konnector.fields &&
+      konnector.fields.advancedFields &&
+      konnector.fields.advancedFields.folderPath) ||
+    (existingAccount === null && konnector.fields && konnector.folderPath)
+  ) {
+    values.folderPath =
+      konnector.fields.advancedFields.folderPath.default ||
+      t('account.config.default_folder')
+    values.namePath = konnector.name
+  }
   return (
     <div className="coz-service-content">
       <AccountConnection
         connector={konnector}
-        existingAccount={existingAccount}
-        fields={konnector.fields}
+        trigger={trigger}
+        values={values}
         {...props}
       />
     </div>
@@ -21,10 +49,8 @@ const CreateAccountService = props => {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    existingAccount: getKonnectorConnectedAccount(
-      state.cozy,
-      ownProps.konnector
-    )
+    existingAccount: getKonnectorConnectedAccount(state, ownProps.konnector),
+    trigger: getTriggerByKonnector(state, ownProps.konnector)
   }
 }
 
