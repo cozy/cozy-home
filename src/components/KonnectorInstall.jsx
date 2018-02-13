445ebb8a4ed8a3f0a11001c80dc278bc49156d5d
CPatchane <code@patchane.com>
2018-02-13 09:45:20 +0100

    fix: split error descriptions components usage into separate file :art:

--- src/components/KonnectorInstall.jsx
+++ src/components/KonnectorInstall.jsx
@@ -10,56 +10,9 @@ import KonnectorSuccess from './KonnectorSuccess'
 import { NavLink } from 'react-router-dom'
 
 import { ACCOUNT_ERRORS } from '../lib/accounts'
+import getErrorDescription from './ErrorDescriptions'
 
 import securityIcon from '../assets/icons/color/icon-cloud-lock.svg'
-
-const KnownErrorDescription = ({ t, connector, errorMessage, message }) => (
-  <DescriptionContent
-    cssClassesObject={{ 'coz-error': true }}
-    title={t(`connection.error.${errorMessage}.title`)}
-    hasError
-    messages={[
-      message ||
-        t(`connection.error.${errorMessage}.description`, {
-          name: connector.name,
-          link: connector.vendorLink
-        })
-    ]}
-  />
-)
-
-const GlobalErrorDescription = ({ t, connector }) => (
-  <DescriptionContent
-    cssClassesObject={{ 'coz-error': true }}
-    title={t('connection.error.default.title')}
-    hasError
-    messages={[
-      t('connection.error.default.description', { name: connector.name })
-    ]}
-  />
-)
-
-const getErrorDescription = props => {
-  const { error } = props
-  switch (error.message) {
-    case ACCOUNT_ERRORS.NOT_EXISTING_DIRECTORY:
-    case ACCOUNT_ERRORS.USER_ACTION_NEEDED:
-    case ACCOUNT_ERRORS.MAINTENANCE:
-      // FIXME temporary, only for EDF
-      if (props.connector && props.connector.slug === 'edf') {
-        return (
-          <KnownErrorDescription
-            errorMessage={error.message}
-            message={props.t && props.t('status.edf_maintenance')}
-            {...props}
-          />
-        )
-      }
-      return <KnownErrorDescription errorMessage={error.message} {...props} />
-    default:
-      return <GlobalErrorDescription {...props} />
-  }
-}
 
 export const KonnectorInstall = ({
   t,
