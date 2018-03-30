99280fca4fc94df3ccb2548d013f48e2d2c50d2a
Grégory Le Garec <gregory.legarec@gmail.com>
2018-03-30 19:21:43 +0200

    feat: remove provider from nav 🔥

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,18 +4,9 @@ import { NavLink as RouterLink, withRouter } from 'react-router-dom'
 import Nav, { NavLink, NavItem, NavIcon, NavText } from 'cozy-ui/react/Nav'
 import UISidebar from 'cozy-ui/react/Sidebar'
 
-import providersIcon from 'assets/icons/icon-stack.svg'
 import connectedIcon from 'assets/icons/icon-pin.svg'
 
-import { OTHERS_CATEGORY } from 'ducks/registry'
-
 export const Sidebar = ({ t, categories, location }) => {
-  let isCategoryView = location.pathname.match(/^\/providers/) !== null
-  const i18nCategories = categories
-    .filter(c => c !== OTHERS_CATEGORY)
-    .map(c => ({ slug: c, label: t(`category.${c}`) }))
-    .sort((a, b) => a.label.localeCompare(b.label))
-
   return (
     <UISidebar>
       <Nav>
@@ -29,53 +20,6 @@ export const Sidebar = ({ t, categories, location }) => {
             <NavText>{t('nav.connected')}</NavText>
           </RouterLink>
         </NavItem>
-        <NavItem>
-          <RouterLink
-            to="/providers/all"
-            activeClassName={NavLink.activeClassName}
-            className={
-              isCategoryView
-                ? `${NavLink.className} ${NavLink.activeClassName}`
-                : NavLink.className
-            }
-          >
-            <NavIcon icon={providersIcon} />
-            <NavText>{t('nav.providers')}</NavText>
-          </RouterLink>
-        </NavItem>
-        {isCategoryView && (
-          <ul className="col-nav-submenu">
-            <li>
-              <RouterLink
-                to="/providers/all"
-                className="col-nav-submenu-link"
-                activeClassName="active"
-              >
-                {t('category.all')}
-              </RouterLink>
-            </li>
-            {i18nCategories.map(category => (
-              <li>
-                <RouterLink
-                  to={`/providers/${category.slug}`}
-                  className="col-nav-submenu-link"
-                  activeClassName="active"
-                >
-                  {category.label}
-                </RouterLink>
-              </li>
-            ))}
-            <li>
-              <RouterLink
-                to={`/providers/${OTHERS_CATEGORY}`}
-                className="col-nav-submenu-link"
-                activeClassName="active"
-              >
-                {t('category.other')}
-              </RouterLink>
-            </li>
-          </ul>
-        )}
       </Nav>
     </UISidebar>
   )
