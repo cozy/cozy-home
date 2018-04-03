05a12616acbc2b1aac3312522b2135e0500866bb
Grégory Le Garec <gregory.legarec@gmail.com>
2018-04-03 15:02:40 +0200

    feat: remove initializeRegistry 🔥

--- src/components/appEntryPoint.jsx
+++ src/components/appEntryPoint.jsx
@@ -1,15 +1,10 @@
 import { cozyConnect } from 'redux-cozy-client'
 
-import { initializeRegistry } from '../ducks/registry'
 import { fetchAccounts } from '../ducks/accounts'
 import { fetchApps } from '../ducks/apps'
 import { fetchKonnectorJobs } from '../ducks/jobs'
 import { fetchKonnectors } from '../ducks/konnectors'
 import { fetchTriggers } from '../ducks/triggers'
-
-const mapActionsToProps = dispatch => ({
-  initializeRegistry: konnectors => dispatch(initializeRegistry(konnectors))
-})
 
 const mapDocumentsToProps = (state, ownProps) => ({
   accounts: fetchAccounts(),
@@ -22,9 +17,6 @@ const mapDocumentsToProps = (state, ownProps) => ({
 })
 
 const appEntryPoint = (WrappedComponent, selectData) =>
-  cozyConnect(mapDocumentsToProps, mapActionsToProps)(
-    WrappedComponent,
-    selectData
-  )
+  cozyConnect(mapDocumentsToProps)(WrappedComponent, selectData)
 
 export default appEntryPoint
