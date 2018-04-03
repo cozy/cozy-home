dc9eb8b5f898283d1d3cb5454443c0c4ec416feb
Grégory Le Garec <gregory.legarec@gmail.com>
2018-04-03 15:15:20 +0200

    feat: remove registry reducer 🔥

--- src/ducks/registry/index.js
+++ src/ducks/registry/index.js
@@ -1,22 +1,2 @@
-import konnectors from './konnectors'
-
 // constant
-const FETCH_REGISTRY_KONNECTORS = 'FETCH_REGISTRY_KONNECTORS'
-const FETCH_REGISTRY_KONNECTORS_SUCCESS = 'FETCH_REGISTRY_KONNECTORS_SUCCESS'
-
 export const OTHERS_CATEGORY = 'others'
-
-// reducers
-const reducer = (state = {}, action) => {
-  switch (action.type) {
-    case FETCH_REGISTRY_KONNECTORS:
-    case FETCH_REGISTRY_KONNECTORS_SUCCESS:
-      return {
-        konnectors: konnectors(state.konnectors, action)
-      }
-    default:
-      return state
-  }
-}
-
-export default reducer
