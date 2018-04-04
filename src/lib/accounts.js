d429db6195a67b84de996221ced5f0033331268d
Grégory Le Garec <gregory.legarec@gmail.com>
2018-04-04 17:46:07 +0200

    feat: huge cleaning of lib/ 🔥

--- src/lib/accounts.js
+++ src/lib/accounts.js
@@ -1,7 +1,4 @@
 /* accounts lib ready to be added to cozy-client-js */
-
-import { KONNECTORS_RESULT_DOCTYPE } from './konnectors'
-
 export const ACCOUNTS_DOCTYPE = 'io.cozy.accounts'
 
 export const ACCOUNT_ERRORS = {
@@ -10,15 +7,6 @@ export const ACCOUNT_ERRORS = {
   NOT_EXISTING_DIRECTORY: 'NOT_EXISTING_DIRECTORY',
   SUCCESS_TIMEOUT: 'SUCCESS_TIMEOUT',
   USER_ACTION_NEEDED: 'USER_ACTION_NEEDED'
-}
-
-export function create(cozy, konnector, auth, folderID, name = '') {
-  return cozy.data.create(ACCOUNTS_DOCTYPE, {
-    name: name,
-    account_type: konnector.slug,
-    auth: auth,
-    folderId: folderID
-  })
 }
 
 export function update(cozy, account, newAccount) {
@@ -35,65 +23,4 @@ export function update(cozy, account, newAccount) {
 
 export function _delete(cozy, account) {
   return cozy.data.delete(ACCOUNTS_DOCTYPE, account)
-}
-
-let cachedAccountsIndex
-function indexAccountsByType(cozy) {
-  return cachedAccountsIndex
-    ? Promise.resolve(cachedAccountsIndex)
-    : cozy.data.defineIndex(ACCOUNTS_DOCTYPE, ['account_type']).then(index => {
-        cachedAccountsIndex = index
-        return Promise.resolve(index)
-      })
-}
-
-function getLastSync(cozy, account) {
-  return (
-    cozy.data
-      .defineIndex(KONNECTORS_RESULT_DOCTYPE, ['account'])
-      .then(index =>
-        cozy.data.query(index, {
-          selector: { account: account._id }
-        })
-      )
-      // FIXME: nosupport for multiple accounts right now
-      .then(results => results[0] && results[0].last_success)
-      .then(
-        lastSync =>
-          !lastSync || /^0001/.test(lastSync) ? null : new Date(lastSync)
-      )
-  )
-}
-
-export function getAccountsByType(cozy, accountType) {
-  if (!accountType) throw new Error('Missing `accountType` parameter')
-
-  return indexAccountsByType(cozy)
-    .then(index =>
-      cozy.data.query(index, {
-        selector: {
-          account_type: accountType
-        }
-      })
-    )
-    .then((accounts = []) =>
-      Promise.all(
-        accounts.map(account =>
-          getLastSync(cozy, account).then(lastSync => {
-            account.lastSync = lastSync
-            return account
-          })
-        )
-      )
-    )
-}
-
-export function getAllAccounts(cozy) {
-  return indexAccountsByType(cozy).then(index => {
-    return cozy.data.query(index, {
-      selector: {
-        account_type: { $gt: null }
-      }
-    })
-  })
 }
--- test/lib/accounts.spec.js
+++ test/lib/accounts.spec.js
@@ -1,37 +1,6 @@
 /* eslint-env jest */
 import * as accounts from '../../src/lib/accounts'
 
-const konnectorMock = {
-  accounts: [],
-  slug: 'mock',
-  name: 'Mock',
-  vendorLink: 'www.mock.cc',
-  dataType: ['bill'],
-  category: 'transport',
-  fields: {
-    login: { type: 'text' },
-    password: { type: 'password' },
-    folderPath: { type: 'folder', advanced: true }
-  }
-}
-
-const folderMock = {
-  _id: '4d1039c9a419779f70d1dee5f200992a',
-  type: 'io.cozy.files',
-  attributes: {
-    created_at: '2017-06-15T15:43:55.132923456Z',
-    dir_id: 'bac3d6ecfe56982d9302f7539c0016f5',
-    name: 'Mock',
-    path: '/Administrative/Mock',
-    tags: [],
-    type: 'directory',
-    updated_at: '2017-06-15T15:43:55.132923456Z'
-  },
-  links: {
-    self: '/files/bac3d6ecfe56982d9302f7539c01b455'
-  }
-}
-
 const accountMock = {
   _id: '4d1039c9a419779f70d1dee5f2009b8f',
   _rev: '6-0cbc3ce9072cc22ccef03f30256a8d57',
@@ -93,40 +62,6 @@ beforeEach(() => {
 })
 
 describe('accounts library', () => {
-  it('should handle account creation', () => {
-    return accounts
-      .create(cozyMock, konnectorMock, accountMock.auth, folderMock._id, 'mock')
-      .then(account => {
-        expect(cozyMock.data.create.mock.calls.length).toBe(1)
-        expect(cozyMock.data.create.mock.calls[0][0]).toBe(
-          accounts.ACCOUNTS_DOCTYPE
-        )
-        expect(cozyMock.data.create.mock.calls[0][1]).toEqual({
-          name: 'mock',
-          account_type: konnectorMock.slug,
-          auth: accountMock.auth,
-          folderId: folderMock._id
-        })
-      })
-  })
-
-  it('should handle account creation without name', () => {
-    return accounts
-      .create(cozyMock, konnectorMock, accountMock.auth, folderMock._id)
-      .then(account => {
-        expect(cozyMock.data.create.mock.calls.length).toBe(1)
-        expect(cozyMock.data.create.mock.calls[0][0]).toBe(
-          accounts.ACCOUNTS_DOCTYPE
-        )
-        expect(cozyMock.data.create.mock.calls[0][1]).toEqual({
-          name: '',
-          account_type: konnectorMock.slug,
-          auth: accountMock.auth,
-          folderId: folderMock._id
-        })
-      })
-  })
-
   it('should handle account updating', () => {
     return accounts
       .update(cozyMock, accountMock, accountMock2)
@@ -147,80 +82,6 @@ describe('accounts library', () => {
         accounts.ACCOUNTS_DOCTYPE
       )
       expect(cozyMock.data.delete.mock.calls[0][1]).toEqual(accountMock)
-    })
-  })
-
-  it('should handle accounts listing by type', () => {
-    const accountType = 'mock'
-    return accounts.getAccountsByType(cozyMock, accountType).then(() => {
-      // indexing
-      expect(cozyMock.data.defineIndex.mock.calls.length).toBe(1)
-      expect(cozyMock.data.defineIndex.mock.calls[0][0]).toBe(
-        accounts.ACCOUNTS_DOCTYPE
-      )
-      expect(cozyMock.data.defineIndex.mock.calls[0][1]).toEqual([
-        'account_type'
-      ])
-      // fetching
-      expect(cozyMock.data.query.mock.calls.length).toBe(1)
-      expect(cozyMock.data.query.mock.calls[0][0]).toBe(indexMock)
-      expect(cozyMock.data.query.mock.calls[0][1]).toEqual({
-        selector: {
-          account_type: accountType
-        }
-      })
-
-      // "pseudo-caching" test
-      return accounts.getAccountsByType(cozyMock, accountType).then(() => {
-        // indexing
-        // caching so no second call to defineIndex here
-        expect(cozyMock.data.defineIndex.mock.calls.length).toBe(1)
-        // fetching
-        expect(cozyMock.data.query.mock.calls.length).toBe(2)
-        expect(cozyMock.data.query.mock.calls[0][0]).toBe(indexMock)
-        expect(cozyMock.data.query.mock.calls[0][1]).toEqual({
-          selector: {
-            account_type: accountType
-          }
-        })
-      })
-    })
-  })
-
-  it('should throw an error if no accountType to get accounts by type', () => {
-    expect(() => {
-      return accounts.getAccountsByType(cozyMock)
-    }).toThrow(new Error('Missing `accountType` parameter'))
-  })
-
-  it('should handle all accounts listing', () => {
-    return accounts.getAllAccounts(cozyMock).then(() => {
-      // indexing
-      // cached index
-      expect(cozyMock.data.defineIndex.mock.calls.length).toBe(0)
-      // fetching
-      expect(cozyMock.data.query.mock.calls.length).toBe(1)
-      expect(cozyMock.data.query.mock.calls[0][0]).toBe(indexMock)
-      expect(cozyMock.data.query.mock.calls[0][1]).toEqual({
-        selector: {
-          account_type: { $gt: null }
-        }
-      })
-
-      // "pseudo-caching" test
-      return accounts.getAllAccounts(cozyMock).then(() => {
-        // indexing
-        // cached index
-        expect(cozyMock.data.defineIndex.mock.calls.length).toBe(0)
-        // fetching
-        expect(cozyMock.data.query.mock.calls.length).toBe(2)
-        expect(cozyMock.data.query.mock.calls[0][0]).toBe(indexMock)
-        expect(cozyMock.data.query.mock.calls[0][1]).toEqual({
-          selector: {
-            account_type: { $gt: null }
-          }
-        })
-      })
     })
   })
 })
