b4b8d64d292f87a5658a38e2fdc065c87490f521
Grégory Le Garec <gregory.legarec@gmail.com>
2018-04-04 17:46:07 +0200

    feat: huge cleaning of lib/ 🔥

--- src/lib/jobs.js
+++ src/lib/jobs.js
@@ -1,37 +1,11 @@
 /* jobs lib ready to be added to cozy-client-js */
-import DateFns from 'date-fns'
 import * as realtime from './realtime'
 
-export const JOBS_DOCTYPE = 'io.cozy.jobs'
-
-export const JOB_STATE = {
-  QUEUED: 'queued',
-  DONE: 'done',
-  ERRORED: 'errored',
-  RUNNING: 'running'
-}
+const JOBS_DOCTYPE = 'io.cozy.jobs'
 
 function decode(job) {
   // Retrieve message properties directly into job
   return { ...job, ...job.message }
-}
-
-export function findById(cozy, id) {
-  return cozy.fetchJSON('GET', `/jobs/${id}`)
-}
-
-export function findQueuedOrRunning(cozy, limitDate) {
-  return cozy
-    .fetchJSON('GET', '/jobs/queue/konnector')
-    .then(results => {
-      return results.map(result => decode(result.attributes))
-    })
-    .then(
-      jobs =>
-        limitDate
-          ? jobs.filter(job => DateFns.isAfter(job.queued_at, limitDate))
-          : jobs
-    )
 }
 
 export function subscribe(cozy, job) {
