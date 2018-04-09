620c042ae5af7fbb1952fb06dd3605f7a908735e
Grégory Le Garec <gregory.legarec@gmail.com>
2018-04-09 15:54:15 +0200

    refacto: use CreateAccountIntent 🔨

--- src/containers/IntentService.jsx
+++ src/containers/IntentService.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import CreateAccountService from '../components/services/CreateAccountService'
-import ServiceBar from '../components/services/ServiceBar'
-
+import CreateAccountIntent from '../components/intents/CreateAccountIntent'
 import { getKonnector } from '../ducks/konnectors'
 
 class IntentService extends Component {
@@ -21,26 +19,12 @@ class IntentService extends Component {
             <p>{t('intent.service.error.cause', { error: error.reason })}</p>
           </div>
         )}
-        <div className="coz-service-layout">
-          <ServiceBar
-            appEditor={appData.cozyAppEditor}
-            appName={appData.cozyAppName}
-            iconPath={`../${appData.cozyIconPath}`}
-            onCancel={() => this.props.onCancel()}
-            closeable={closeable}
-            {...this.context}
-          />
-          {konnector && (
-            <CreateAccountService
-              konnector={konnector}
-              onCancel={() => onCancel()}
-              onSuccess={account => onTerminate(account)}
-              disableSuccessTimeout={disableSuccessTimeout}
-              closeModal={() => this.onCancel()}
-              {...this.context}
-            />
-          )}
-        </div>
+        <CreateAccountIntent
+          appData={appData}
+          konnector={konnector}
+          onCancel={() => onCancel()}
+          onSuccess={account => onTerminate(account)}
+        />
       </div>
     )
   }
