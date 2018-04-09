743f6ae9a693d87c903d53b5002667768c2f33fb
Grégory Le Garec <gregory.legarec@gmail.com>
2018-04-09 15:20:27 +0200

    feat: IntentService now deals with only one konnector 📝

--- src/containers/IntentService.jsx
+++ src/containers/IntentService.jsx
@@ -7,53 +7,9 @@ import ServiceBar from '../components/services/ServiceBar'
 import { getKonnector } from '../ducks/konnectors'
 
 class IntentService extends Component {
-  constructor(props) {
-    super(props)
-
-    if (props.konnectors && props.konnectors.length === 1) {
-      this.state = {
-        konnector: props.konnectors[0],
-        isUnique: true
-      }
-    }
-  }
-
-  reset() {
-    this.setState({
-      konnector: null
-    })
-  }
-
-  onSuccess(account) {
-    const { isUnique } = this.state
-
-    if (isUnique) {
-      this.reset()
-      return this.props.onTerminate(account)
-    }
-
-    this.reset()
-  }
-
-  onCancel() {
-    const { isUnique } = this.state
-
-    if (isUnique) {
-      return this.props.onCancel()
-    }
-
-    this.reset()
-  }
-
-  showKonnector(konnector) {
-    this.setState({
-      konnector: konnector
-    })
-  }
-
   render() {
-    const { appData, konnectors, onCancel } = this.props
-    const { konnector, error, disableSuccessTimeout, closeable } = this.state
+    const { appData, konnector, onCancel, onTerminate } = this.props
+    const { error } = this.state
 
     const { t } = this.context
 
@@ -70,19 +26,15 @@ class IntentService extends Component {
             appEditor={appData.cozyAppEditor}
             appName={appData.cozyAppName}
             iconPath={`../${appData.cozyIconPath}`}
-            onCancel={onCancel}
+            onCancel={() => this.props.onCancel()}
             closeable={closeable}
-            hasReturnToKonnectorsListButton={
-              !error && konnectors.length > 1 && konnector
-            }
-            returnToKonnectorsList={() => this.setState({ konnector: null })}
             {...this.context}
           />
           {konnector && (
             <CreateAccountService
               konnector={konnector}
-              onCancel={() => this.onCancel()}
-              onSuccess={account => this.onSuccess(account)}
+              onCancel={() => onCancel()}
+              onSuccess={account => onTerminate(account)}
               disableSuccessTimeout={disableSuccessTimeout}
               closeModal={() => this.onCancel()}
               {...this.context}
@@ -98,7 +50,7 @@ const mapStateToProps = (state, ownProps) => {
   const { data } = ownProps
   const { slug } = data
   return {
-    konnectors: (slug && [getKonnector(state.cozy, slug)]) || []
+    konnector: slug && getKonnector(state.cozy, slug)
   }
 }
 
