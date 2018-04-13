c16e94d96c74f012da4408da9cc7f746ed562de8
CPatchane <code@patchane.com>
2018-04-13 14:11:07 +0200

    feat: add redirection to store using REDIRECT intent :sparkles:

--- src/components/appEntryPoint.jsx
+++ src/components/appEntryPoint.jsx
@@ -1,14 +1,12 @@
 import { cozyConnect } from 'redux-cozy-client'
 
 import { fetchAccounts } from '../ducks/accounts'
-import { fetchApps } from '../ducks/apps'
 import { fetchKonnectorJobs } from '../ducks/jobs'
 import { fetchKonnectors } from '../ducks/konnectors'
 import { fetchTriggers } from '../ducks/triggers'
 
 const mapDocumentsToProps = (state, ownProps) => ({
   accounts: fetchAccounts(),
-  apps: fetchApps(),
   jobs: fetchKonnectorJobs(),
   konnectors: fetchKonnectors(),
   triggers: fetchTriggers()
