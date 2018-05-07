8adff5dea6c3607f5987cd57ca9ee9c4960a542a
Grégory Le Garec <gregory.legarec@gmail.com>
2018-05-07 15:21:34 +0200

    🚑 feat: prevent missing icon error

--- src/components/DataItem.jsx
+++ src/components/DataItem.jsx
@@ -4,16 +4,29 @@ import React from 'react'
 import { translate } from 'cozy-ui/react/I18n'
 import Icon from 'cozy-ui/react/Icon'
 
-export const DataItem = ({ t, dataType }) => (
-  <li className={styles['col-data-item']}>
-    <Icon
-      className={styles['col-data-item-icon']}
-      icon={require('../assets/sprites/icon-' + dataType + '.svg').default}
-      width="32px"
-      height="32px"
-    />
-    {t(`dataType.${dataType}`)}
-  </li>
-)
+export const DataItem = ({ t, dataType }) => {
+  let icon
+  try {
+    icon = require('../assets/sprites/icon-' + dataType + '.svg')
+  } catch (error) {
+    console.warn(
+      `Unable to get icon for data type ${dataType}, reason: ${error.message}`
+    )
+  }
+
+  return (
+    <li className={styles['col-data-item']}>
+      {icon && (
+        <Icon
+          className={styles['col-data-item-icon']}
+          icon={icon.default}
+          width="32px"
+          height="32px"
+        />
+      )}
+      {t(`dataType.${dataType}`)}
+    </li>
+  )
+}
 
 export default translate()(DataItem)
