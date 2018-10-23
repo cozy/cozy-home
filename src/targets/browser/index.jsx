6228fc2f86b7e6439d6caad17fa57b51aa938c47
CPatchane <code@patchane.com>
2018-10-23 13:41:47 +0200

    fix: :bug: Use intro.js with position fix

--- src/targets/browser/index.jsx
+++ src/targets/browser/index.jsx
@@ -14,7 +14,7 @@ import PiwikHashRouter from 'lib/PiwikHashRouter'
 import collectConfig from 'config/collect'
 import configureStore from 'store/configureStore'
 
-import 'intro.js/minified/introjs.min.css'
+import 'intro.js-fix-cozy/minified/introjs.min.css'
 import 'styles/index.styl'
 
 const lang = document.documentElement.getAttribute('lang') || 'en'
