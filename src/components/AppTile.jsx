a2bbf1e0f9aa153a6807a1e159823ca15bbaa949
Grégory Le Garec <gregory.legarec@gmail.com>
2018-10-26 11:17:48 +0200

    refactor: Move fetchIcon to icon lib 🔨

--- src/components/AppTile.jsx
+++ src/components/AppTile.jsx
@@ -2,23 +2,7 @@ import React, { Component } from 'react'
 
 import { translate } from 'cozy-ui/react/I18n'
 import AppIcon from 'cozy-ui/react/AppIcon'
-
-const fetchIcon = cozyClient => async url => {
-  let icon
-  try {
-    const resp = await cozyClient.fetch('GET', url)
-    if (!resp.ok)
-      throw new Error(`Error while fetching icon ${resp.statusText}: ${url}`)
-    icon = await resp.blob()
-  } catch (error) {
-    throw error
-  }
-  const mimeType = icon.type
-  if (mimeType && mimeType.match(/^image\/.*$/)) {
-    return URL.createObjectURL(icon)
-  }
-  throw new Error(`App icon ${url} is not an image.`)
-}
+import { fetchIcon } from 'lib/icons'
 
 export class AppTile extends Component {
   render() {
--- src/lib/icons.js
+++ src/lib/icons.js
@@ -33,3 +33,20 @@ export const getKonnectorIcon = konnector => {
   }
   return icon
 }
+
+export const fetchIcon = cozyClient => async url => {
+  let icon
+  try {
+    const resp = await cozyClient.fetch('GET', url)
+    if (!resp.ok)
+      throw new Error(`Error while fetching icon ${resp.statusText}: ${url}`)
+    icon = await resp.blob()
+  } catch (error) {
+    throw error
+  }
+  const mimeType = icon.type
+  if (mimeType && mimeType.match(/^image\/.*$/)) {
+    return URL.createObjectURL(icon)
+  }
+  throw new Error(`App icon ${url} is not an image.`)
+}
