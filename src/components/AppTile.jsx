1a53718b80f958293258b8e7391a8cdc615fa55b
Grégory Le Garec <gregory.legarec@gmail.com>
2018-11-27 01:08:42 +0100

    feat(AppIcon): pass domain and secure 📝

--- src/components/AppTile.jsx
+++ src/components/AppTile.jsx
@@ -2,12 +2,11 @@ import React, { Component } from 'react'
 
 import { translate } from 'cozy-ui/react/I18n'
 import AppIcon from 'cozy-ui/react/AppIcon'
-import { fetchIcon } from 'lib/icons'
+import { appIconProps } from 'lib/icons'
 
 export class AppTile extends Component {
   render() {
     const { app, t } = this.props
-    const { client } = this.context
     const displayName = app.name_prefix
       ? `${app.name_prefix} ${app.name}`
       : app.name
@@ -17,7 +16,7 @@ export class AppTile extends Component {
           <AppIcon
             alt={t('app.logo.alt', { name: displayName })}
             app={app}
-            fetchIcon={fetchIcon(client, app)}
+            {...appIconProps}
           />
         </div>
         <h3 className="item-title">{displayName}</h3>
