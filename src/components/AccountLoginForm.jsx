1a7a4aaebc5c1d7c5ecfab4ad97cf9f0788a0c33
Grégory Le Garec <gregory.legarec@gmail.com>
2019-01-03 10:20:14 +0100

    feat: Form from Cozy-Harvest (feature) ✨

--- src/components/AccountLoginForm.jsx
+++ src/components/AccountLoginForm.jsx
@@ -1,11 +1,25 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+
+import { AccountForm } from 'cozy-harvest-lib'
 
 import LegacyAccountLoginForm from './LegacyAccountLoginForm'
+import { getKonnector } from 'ducks/konnectors'
 
 export class AccountLoginForm extends PureComponent {
   render() {
-    return <LegacyAccountLoginForm {...this.props} />
+    const { features } = this.context
+    const { konnector } = this.props
+    return features && features.includes('harvest') ? (
+      <AccountForm {...konnector} />
+    ) : (
+      <LegacyAccountLoginForm {...this.props} />
+    )
   }
 }
 
-export default AccountLoginForm
+const mapStateToProps = (state, ownProps) => ({
+  konnector: getKonnector(state.cozy, ownProps.connectorSlug)
+})
+
+export default connect(mapStateToProps)(AccountLoginForm)
