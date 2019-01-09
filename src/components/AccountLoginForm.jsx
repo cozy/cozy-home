bf70d7c3a9cc2f45a9f2f5fc35cd9472cc20d66c
Grégory Le Garec <gregory.legarec@gmail.com>
2019-01-09 14:42:20 +0100

    feat: Use cozy-flags for Harvest ✨

--- src/components/AccountLoginForm.jsx
+++ src/components/AccountLoginForm.jsx
@@ -2,16 +2,16 @@ import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 
 import { AccountForm } from 'cozy-harvest-lib'
+import flag from 'cozy-flags'
 
 import LegacyAccountLoginForm from './LegacyAccountLoginForm'
 import { getKonnector } from 'ducks/konnectors'
 
 export class AccountLoginForm extends PureComponent {
   render() {
-    const { features } = this.context
     const { account, konnector } = this.props
     const initialValues = account ? account.auth : {}
-    return features && features.includes('harvest') ? (
+    return flag('harvest') ? (
       <AccountForm initialValues={initialValues} {...konnector} />
     ) : (
       <LegacyAccountLoginForm {...this.props} />
