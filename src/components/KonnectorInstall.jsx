da0d9cbba5daf99c40d331525b7e9f1c564579d6
Grégory Le Garec <gregory.legarec@gmail.com>
2019-03-04 17:59:25 +0100

    feat(harvest): Show Maintenance message 📝

--- src/components/KonnectorInstall.jsx
+++ src/components/KonnectorInstall.jsx
@@ -4,22 +4,38 @@ import { connect } from 'react-redux'
 import { TriggerManager } from 'cozy-harvest-lib'
 import flag from 'cozy-flags'
 
+import KonnectorMaintenance from './KonnectorMaintenance'
 import LegacyKonnectorInstall from './LegacyKonnectorInstall'
 import { getKonnector } from 'ducks/konnectors'
 import styles from '../styles/konnectorInstall'
 
 export class KonnectorInstall extends PureComponent {
   render() {
-    const { account, konnector, onDone, onLoginSuccess } = this.props
+    const {
+      account,
+      konnector,
+      lang,
+      maintenance,
+      onDone,
+      onLoginSuccess
+    } = this.props
     return flag('harvest') ? (
       <div className={styles['col-account-connection-content']}>
         <div className={styles['col-account-connection-form']}>
-          <TriggerManager
-            account={account}
-            konnector={konnector}
-            onLoginSuccess={onLoginSuccess}
-            onDone={onDone}
-          />
+          {maintenance && maintenance.longTerm ? (
+            <KonnectorMaintenance
+              maintenance={maintenance}
+              lang={lang}
+              konnectorName={konnector.name}
+            />
+          ) : (
+            <TriggerManager
+              account={account}
+              konnector={konnector}
+              onLoginSuccess={onLoginSuccess}
+              onDone={onDone}
+            />
+          )}
         </div>
       </div>
     ) : (
