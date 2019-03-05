df192c1e040740e1d5ffcc155af85ee4c52de19c
Grégory Le Garec <gregory.legarec@gmail.com>
2019-03-05 23:14:02 +0100

    feat: Rewrite KonnectorFolder as standalone component ✊
    Should have been done a long time ago in my humble opinion.

--- src/components/KonnectorFolder.jsx
+++ src/components/KonnectorFolder.jsx
@@ -1,28 +1,61 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import classNames from 'classnames'
+
+import Icon from 'cozy-ui/react/Icon'
 import { translate } from 'cozy-ui/react/I18n'
 
 import styles from '../styles/konnectorFolder'
 
 import DescriptionContent from './DescriptionContent'
+import { getApp } from '../ducks/apps'
 
-class KonnectorFolder extends React.Component {
-  render({ t, driveUrl, trigger }) {
+/**
+ * Renders a link only if href prop is provided
+ */
+class MaybeLink extends PureComponent {
+  render() {
+    const { className, href } = this.props
+    return href ? (
+      <a className={className} href={href}>
+        {this.props.children}
+      </a>
+    ) : (
+      <span className={className}>{this.props.children}</span>
+    )
+  }
+}
+
+export class KonnectorFolder extends PureComponent {
+  render() {
+    const { driveApp, t, trigger } = this.props
+    const disabled = !driveApp
     return (
       <div className={styles['col-account-folder']}>
         <DescriptionContent title={t('account.folder.withoutSettings.title')}>
-          {driveUrl && (
-            <a
-              className={styles['col-account-folder-link']}
-              href={`${driveUrl}${trigger.message.folder_to_save}`}
-            >
-              {t('account.folder.link')}
-            </a>
-          )}
+          <MaybeLink
+            className={classNames(styles['col-account-folder-link'], {
+              'u-silver': disabled,
+              'u-c-not-allowed': disabled
+            })}
+            href={
+              driveApp &&
+              `${driveApp.links.related}#/files/${
+                trigger.message.folder_to_save
+              }`
+            }
+          >
+            <Icon className="u-mr-half" icon="openwith" />
+            {t('account.folder.link')}
+          </MaybeLink>
         </DescriptionContent>
-        )
       </div>
     )
   }
 }
 
-export default translate()(KonnectorFolder)
+const mapStateToProps = state => ({
+  driveApp: getApp(state.apps, 'drive')
+})
+
+export default connect(mapStateToProps)(translate()(KonnectorFolder))
