78ff4374c312012373d5bddec0fbcb9f85e01023
Grégory Le Garec <gregory.legarec@gmail.com>
2019-03-06 14:20:19 +0100

    refactor: Use PureComponents in Applications 🛠

--- src/components/Applications.jsx
+++ src/components/Applications.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 
 import AppTile from './AppTile'
 import LoadingPlaceholder from './LoadingPlaceholder'
@@ -6,60 +6,65 @@ import fillWithGhostItems from './helpers/fillWithGhostItems'
 import { Query } from 'cozy-client'
 import { translate } from 'cozy-ui/react/I18n'
 
-const LoadingAppTiles = ({ num }) => {
-  const tiles = []
-  for (let i = 0; i < num; i++) {
-    tiles.push(
-      <div className="item-wrapper">
-        <header className="item-header">
-          <div className="item-icon">
+class LoadingAppTiles extends PureComponent {
+  render() {
+    const { num } = this.props
+    const tiles = []
+    for (let i = 0; i < num; i++) {
+      tiles.push(
+        <div className="item-wrapper">
+          <header className="item-header">
+            <div className="item-icon">
+              <LoadingPlaceholder />
+            </div>
+          </header>
+          <h3 className="item-title">
             <LoadingPlaceholder />
-          </div>
-        </header>
-        <h3 className="item-title">
-          <LoadingPlaceholder />
-        </h3>
+          </h3>
+        </div>
+      )
+    }
+    return (
+      <div className="list app-list">
+        {tiles}
+        {fillWithGhostItems(6)}
       </div>
     )
   }
-  return (
-    <div className="list app-list">
-      {tiles}
-      {fillWithGhostItems(6)}
-    </div>
-  )
 }
 
-export const Applications = () => {
-  const ignoredAppSlugs = ['home', 'settings']
-  return (
-    <div className="app-list-wrapper">
-      <Query
-        query={client => {
-          return client.all('io.cozy.apps')
-        }}
-      >
-        {({ data, fetchStatus }) => {
-          return fetchStatus !== 'loaded' ? (
-            <LoadingAppTiles num="3" />
-          ) : (
-            <div className="list app-list" data-tutorial="home-apps">
-              {data
-                .filter(
-                  app =>
-                    app.state !== 'hidden' &&
-                    !ignoredAppSlugs.includes(app.slug)
-                )
-                .map((app, index) => (
-                  <AppTile key={index} app={app} />
-                ))}
-              {fillWithGhostItems(6)}
-            </div>
-          )
-        }}
-      </Query>
-    </div>
-  )
+export class Applications extends PureComponent {
+  render() {
+    const ignoredAppSlugs = ['home', 'settings']
+    return (
+      <div className="app-list-wrapper">
+        <Query
+          query={client => {
+            return client.all('io.cozy.apps')
+          }}
+        >
+          {({ data, fetchStatus }) => {
+            return fetchStatus !== 'loaded' ? (
+              <LoadingAppTiles num="3" />
+            ) : (
+              <div className="list app-list" data-tutorial="home-apps">
+                {data
+                  .filter(
+                    app =>
+                      app.state !== 'hidden' &&
+                      !ignoredAppSlugs.includes(app.slug)
+                  )
+                  .map((app, index) => (
+                    <AppTile key={index} app={app} />
+                  ))}
+                {fillWithGhostItems(6)}
+              </div>
+            )
+          }}
+        </Query>
+      </div>
+    )
+  }
 }
 
 export default translate()(Applications)
