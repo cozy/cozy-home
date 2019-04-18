3d00bda8cfc2d952b9e5a0024de36f0acdd0d440
Patrick Browne <pt.browne@gmail.com>
2019-04-18 21:08:31 +0200

    refactor: Extract SuccessLinks

--- src/components/KonnectorSuccess.jsx
+++ src/components/KonnectorSuccess.jsx
@@ -18,6 +18,11 @@ const SuccessImage = () => (
   </div>
 )
 
+const SuccessLinks = ({ children }) => (
+  <p className={classNames(styles['col-account-success-links'], 'u-mv-half')}>
+    {children}
+  </p>
+)
 
 const DriveLink = translate()(({ folderId, t }) => (
   <TriggerFolderLink
@@ -65,15 +70,10 @@ export class KonnectorSuccess extends Component {
             messages={!error && messages}
           >
             {hasLinks && (
-              <p
-                className={classNames(
-                  styles['col-account-success-links'],
-                  'u-mv-half'
-                )}
-              >
-              </p>
+              <SuccessLinks>
                 {displayDriveUrl && <DriveLink folderId={trigger.message.folder_to_save} />}
                 {displayBanksUrl && <BanksLink banksUrl={banksUrl} />}
+              </SuccessLinks>
             )}
           </DescriptionContent>
 
