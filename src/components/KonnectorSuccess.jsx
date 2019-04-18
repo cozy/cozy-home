da3e174579b6e9c23a9d469f853fd3f62f929c1f
Patrick Browne <pt.browne@gmail.com>
2019-04-18 21:28:38 +0200

    fixup: extract SuccessFooter

--- src/components/KonnectorSuccess.jsx
+++ src/components/KonnectorSuccess.jsx
@@ -72,6 +72,12 @@ const DriveLink = translate()(({ folderId, t }) => (
   />
 ))
 
+const SuccessFooter = translate()(({ children }) => (
+  <div className={styles['coz-form-controls-success']}>
+    <div className={styles['col-account-form-success-buttons']}>{children}</div>
+  </div>
+))
+
 export class KonnectorSuccess extends Component {
   constructor(props, context) {
     super(props, context)
@@ -118,17 +124,15 @@ export class KonnectorSuccess extends Component {
             )}
           </DescriptionContent>
 
-          <div className={styles['coz-form-controls-success']}>
-            <div className={styles['col-account-form-success-buttons']}>
-              <Button
-                label={successButtonLabel || t('account.success.button')}
-                onClick={event => {
-                  event.preventDefault()
-                  onDone(account)
-                }}
-              />
-            </div>
-          </div>
+          <SuccessFooter>
+            <Button
+              label={successButtonLabel || t('account.success.button')}
+              onClick={event => {
+                event.preventDefault()
+                onDone(account)
+              }}
+            />
+          </SuccessFooter>
         </div>
       )
     )
