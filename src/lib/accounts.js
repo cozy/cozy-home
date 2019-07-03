a1fbc594158b9de64f46d77e59134efbf903ac6e
Grégory Le Garec <gregory.legarec@gmail.com>
2019-07-03 18:11:59 +0200

    feat: Clean accounts lib 🔥

--- src/lib/accounts.js
+++ src/lib/accounts.js
@@ -3,14 +3,6 @@ import * as realtime from './realtime'
 
 export const ACCOUNTS_DOCTYPE = 'io.cozy.accounts'
 
-export const ACCOUNT_ERRORS = {
-  LOGIN_FAILED: 'LOGIN_FAILED',
-  MAINTENANCE: 'MAINTENANCE',
-  NOT_EXISTING_DIRECTORY: 'NOT_EXISTING_DIRECTORY',
-  SUCCESS_TIMEOUT: 'SUCCESS_TIMEOUT',
-  USER_ACTION_NEEDED: 'USER_ACTION_NEEDED'
-}
-
 // Order matters
 export const probableLoginFieldNames = [
   'login',
@@ -18,36 +10,6 @@ export const probableLoginFieldNames = [
   'new_identifier',
   'email'
 ]
-
-function ignorePassword(auth) {
-  const sanitized = { ...auth }
-  delete sanitized.password
-  return sanitized
-}
-
-function sanitizeAccountAuthForUpdate(auth) {
-  const sanitized = { ...auth }
-  delete sanitized.credentials_encrypted
-  // Always ignore empty passwords on account update
-  const { password } = auth
-  if (typeof password !== 'undefined' && !password) {
-    return ignorePassword(sanitized)
-  }
-  return sanitized
-}
-
-export function update(cozy, account, newAccount) {
-  return cozy.data.updateAttributes(ACCOUNTS_DOCTYPE, account._id, {
-    auth: {
-      ...ignorePassword(account.auth),
-      ...sanitizeAccountAuthForUpdate(newAccount.auth)
-    }
-  })
-}
-
-export function _delete(cozy, account) {
-  return cozy.data.delete(ACCOUNTS_DOCTYPE, account)
-}
 
 export function subscribeAll(cozy) {
   return realtime.subscribeAll(cozy, ACCOUNTS_DOCTYPE)
