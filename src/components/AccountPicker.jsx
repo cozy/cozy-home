97871d9974c0d471d432d4bfe965dde5efa968f3
Quentin Valmori <quentin.valmori@gmail.com>
2019-08-01 15:29:00 +0200

    feat: Use HarvestModal

--- src/components/AccountPicker.jsx
+++ src/components/AccountPicker.jsx
@@ -1,74 +1,51 @@
-import React from 'react'
-import classNames from 'classnames'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { NavLink, Redirect, withRouter } from 'react-router-dom'
-
+import { Redirect } from 'react-router-dom'
 import flow from 'lodash/flow'
 
-import { translate } from 'cozy-ui/react/I18n'
-import Icon from 'cozy-ui/react/Icon'
-import Modal, { ModalHeader, ModalContent } from 'cozy-ui/react/Modal'
-
-import AccountPickerItem from 'components/AccountPickerItem'
-import KonnectorHeaderIcon from 'components/KonnectorHeaderIcon'
-import backIcon from 'assets/sprites/icon-arrow-left.svg'
+import { KonnectorModal } from 'cozy-harvest-lib'
 import { getKonnector } from 'ducks/konnectors'
-import { hasPendingUpdate } from 'lib/konnectors'
-import styles from 'styles/accountPicker'
-import { getConnectionsByKonnector } from 'reducers'
 
-import { withTriggersAndAccounts, KonnectorModal } from 'cozy-harvest-lib'
-export const AccountPicker = ({
-  t,
-  connections,
-  history,
-  konnector,
-  match,
-  konnectorWithTriggers
-}) => {
-  const { konnectorSlug } = match.params
-  if (!connections.length)
-    return <Redirect to={`/connected/${konnector.slug}/new`} />
-  return <KonnectorModal konnector={konnectorWithTriggers} />
-  /*return (
-    <Modal dismissAction={() => history.push('/connected')} mobileFullscreen>
-      <ModalHeader>
-        <div className="col-account-connection-header">
-          <NavLink to="/connected" className="col-account-connection-back">
-            <Icon icon={backIcon} />
-          </NavLink>
-          <KonnectorHeaderIcon konnector={konnector} />
-        </div>
-      </ModalHeader>
-      <ModalContent>
-        <ul className={styles[classNames('col-account-picker')]}>
-          {connections.map((connection, index) => (
-            <li key={index}>
-              <AccountPickerItem
-                konnectorSlug={konnectorSlug}
-                connection={connection}
-                hasUpdate={hasPendingUpdate(konnector)}
-              />
-            </li>
-          ))}
-          <li>
-            <NavLink
-              to={`/connected/${konnectorSlug}/new`}
-              className={classNames(
-                styles['col-account-picker-button'],
-                styles['col-account-picker-button-add']
-              )}
-            >
-              <span>
-                <Icon icon="plus" />
-                <span>{t('account_picker.add_account_button.label')}</span>
-              </span>
-            </NavLink>
-          </li>
-        </ul>
-      </ModalContent>
-    </Modal>
-  )*/
+import { getConnectionsByKonnector } from 'reducers'
+import { withClient } from 'cozy-client/dist/hoc'
+
+class AccountPicker extends Component {
+  state = {
+    triggersWithStatus: []
+  }
+  async componentDidMount() {
+    const { client, konnector } = this.props
+    const triggers = await client.query(
+      client
+        .find('io.cozy.triggers')
+        .where({
+          message: {
+            konnector: konnector.slug
+          },
+          worker: 'konnector'
+        })
+        .indexFields(['message.konnector', 'worker'])
+        .sortBy([{ 'message.konnector': 'desc' }, { worker: 'desc' }])
+    )
+    const triggersWithStatus = await Promise.all(
+      triggers.data.map(async trigger => {
+        const triggerWithStatus = await client.query(
+          client.get('io.cozy.triggers', trigger._id)
+        )
+        return triggerWithStatus.data
+      })
+    )
+    this.setState({ triggersWithStatus: { data: triggersWithStatus } })
+  }
+  render() {
+    const { connections, konnector } = this.props
+    const { triggersWithStatus } = this.state
+    const konnectorWithtriggers = { ...konnector, triggers: triggersWithStatus }
+    if (!connections.length)
+      return <Redirect to={`/connected/${konnector.slug}/new`} />
+    if (triggersWithStatus.length === 0) return 'loading'
+    return <KonnectorModal konnector={konnectorWithtriggers} />
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
@@ -81,9 +58,7 @@ const mapStateToProps = (state, ownProps) => {
 
 export default flow(
   connect(mapStateToProps),
-  withTriggersAndAccounts,
-  withRouter,
-  translate()
+  withClient
 )(AccountPicker)
 
 //export default (translate()(withRouter()))
