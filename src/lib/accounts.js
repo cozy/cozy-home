adbb34aec0b61988a2f0dc2ea2e1a9db94a7bafb
Quentin Valmori <quentin.valmori@gmail.com>
2019-08-02 14:53:19 +0200

    refactor: Remove useless code

--- src/lib/accounts.js
+++ src/lib/accounts.js
@@ -3,14 +3,6 @@ import * as realtime from './realtime'
 
 export const ACCOUNTS_DOCTYPE = 'io.cozy.accounts'
 
-// Order matters
-export const probableLoginFieldNames = [
-  'login',
-  'identifier',
-  'new_identifier',
-  'email'
-]
-
 export function subscribeAll(cozy) {
   return realtime.subscribeAll(cozy, ACCOUNTS_DOCTYPE)
 }
