2a6b87abb78f1fd472a7641dffb02037d30d5c5f
Yannick Lohse <contact@yannick-lohse.fr>
2019-08-21 18:47:30 +0200

    feat: Harvest routes

---
+++ src/components/Konnector.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Redirect, withRouter } from 'react-router-dom'
+import flow from 'lodash/flow'
+
+import { Routes as HarvestRoutes } from 'cozy-harvest-lib'
+import { getKonnector } from 'ducks/konnectors'
+
+import { getConnectionsByKonnector, getTriggersByKonnector } from 'reducers'
+import { withClient } from 'cozy-client/dist/hoc'
+
+class Konnector extends Component {
+  render() {
+    const { connections, konnector, history, match, triggers } = this.props
+    const konnectorWithtriggers = { ...konnector, triggers: triggers }
+    const selectedAccountId = match.params.accountId
+
+    if (!connections.length)
+      return <Redirect to={`/connected/${konnector.slug}/new`} />
+
+    return (
+      <HarvestRoutes
+        base="/connected/:konnectorSlug"
+        konnector={konnectorWithtriggers}
+      />
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const { konnectorSlug } = ownProps.match.params
+  return {
+    connections: getConnectionsByKonnector(state, konnectorSlug),
+    konnector: getKonnector(state.cozy, konnectorSlug),
+    triggers: getTriggersByKonnector(state, konnectorSlug)
+  }
+}
+
+export default flow(
+  connect(mapStateToProps),
+  withClient,
+  withRouter
+)(Konnector)
