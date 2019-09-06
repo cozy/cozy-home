748f0be99e14e1ac433c95249bb0ea9ce99349eb
Quentin Valmori <quentin.valmori@gmail.com>
2019-09-06 11:19:49 +0200

    fix: Lint issues

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 import classNames from 'classnames'
-import { Route, Switch, withRouter, Redirect } from 'react-router-dom'
+import { Route, withRouter } from 'react-router-dom'
 
 import { translate } from 'cozy-ui/react/I18n'
 import { Main, Content } from 'cozy-ui/react/Layout'
 
-import AccountPicker from 'components/AccountPicker'
 import Konnector from 'components/Konnector'
 import Applications from 'components/Applications'
-import ConnectionManagement from 'containers/ConnectionManagement'
 import ScrollToTopOnMount from 'components/ScrollToTopOnMount'
 import Services from 'components/Services'
 import { isTutorial, display as displayTutorial } from 'lib/tutorial'
@@ -36,7 +34,7 @@ class Home extends Component {
   }
 
   render() {
-    const { wrapper, match } = this.props
+    const { wrapper } = this.props
     return (
       <Main>
         <ScrollToTopOnMount target={wrapper} />
@@ -51,23 +49,6 @@ class Home extends Component {
           </div>
         </Content>
         <Route path="/connected/:konnectorSlug" component={Konnector} />
-        {/* <Switch>
-          <Route
-            exact
-            path="/connected/:konnectorSlug"
-            component={AccountPicker}
-          />
-          <Route
-            path="/connected/:konnectorSlug/new"
-            render={props => (
-              <ConnectionManagement originPath="/connected" {...props} />
-            )}
-          />
-          <Route
-            path="/connected/:konnectorSlug/accounts/:accountId"
-            component={AccountPicker}
-          />
-        </Switch> */}
       </Main>
     )
   }
