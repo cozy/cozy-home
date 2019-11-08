466f4e3a46af53412f61d28fcc54952c372f9578
Yannick Lohse <contact@yannick-lohse.fr>
2019-11-08 09:47:57 +0100

    fix: Add links to candidates

--- src/components/CandidateServiceTile.jsx
+++ src/components/CandidateServiceTile.jsx
@@ -2,23 +2,41 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { translate } from 'cozy-ui/react/I18n'
 import AppIcon from 'cozy-ui/react/AppIcon'
+import AppLinker, { generateWebLink } from 'cozy-ui/react/AppLinker'
+import { withClient } from 'cozy-client'
 
 class CandidateServiceTile extends React.Component {
   render() {
-    const { t, slug, name } = this.props
+    const { t, slug, name, client } = this.props
     const { domain, secure } = this.context
+    const cozyURL = new URL(client.getStackClient().uri)
+    const app = 'store'
+    const nativePath = `/discover/${slug}`
+
     return (
-      <div className="item item--ghost">
-        <div className="item-icon">
-          <AppIcon
-            alt={t('app.logo.alt', { name })}
-            app={slug}
-            domain={domain}
-            secure={secure}
-          />
-        </div>
-        <span className="item-title">{name}</span>
-      </div>
+      <AppLinker
+        slug={app}
+        nativePath={nativePath}
+        href={generateWebLink({
+          cozyUrl: cozyURL.origin,
+          slug: app,
+          nativePath: nativePath
+        })}
+      >
+        {({ onClick, href }) => (
+          <a onClick={onClick} href={href} className="item item--ghost">
+            <div className="item-icon">
+              <AppIcon
+                alt={t('app.logo.alt', { name })}
+                app={slug}
+                domain={domain}
+                secure={secure}
+              />
+            </div>
+            <span className="item-title">{name}</span>
+          </a>
+        )}
+      </AppLinker>
     )
   }
 }
@@ -27,7 +45,8 @@ CandidateServiceTile.contextTypes = {
   slug: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   domain: PropTypes.string.isRequired,
+  client: PropTypes.object.isRequired,
   secure: PropTypes.bool
 }
 
-export default translate()(CandidateServiceTile)
+export default translate()(withClient(CandidateServiceTile))
