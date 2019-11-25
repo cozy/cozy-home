bf2c37a2012761b9fb850a4986d628782826f971
Yannick Lohse <contact@yannick-lohse.fr>
2019-11-25 13:49:39 +0100

    refactor: Use modal from harvest

--- src/components/CandidateServiceTile.jsx
+++ src/components/CandidateServiceTile.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import get from 'lodash/get'
 import { translate } from 'cozy-ui/react/I18n'
+import Alerter from 'cozy-ui/react/Alerter'
 import { withClient } from 'cozy-client'
 import AppIcon from 'components/AppIcon'
 import useRegistryInformation from 'hooks/useRegistryInformation'
-import KonnectorSuggestionModal from 'components/KonnectorSuggestionModal'
+import { KonnectorSuggestionModal } from 'cozy-harvest-lib'
 
 const CandidateServiceTile = ({ t, client, konnector }) => {
   const { slug } = konnector
@@ -13,28 +14,37 @@ const CandidateServiceTile = ({ t, client, konnector }) => {
   const name = registryData
     ? get(registryData, 'latest_version.manifest.name', slug)
     : ''
-  const [isModalDisplayed, setModalDisplayed] = useState(true)
+  const [isModalDisplayed, setModalDisplayed] = useState(false)
 
   return (
-    <div className="item item--ghost" onClick={() => setModalDisplayed(true)}>
+    <>
       {isModalDisplayed && (
         <KonnectorSuggestionModal
-          konnector={konnector}
-          closeModal={() => setModalDisplayed(false)}
+          konnectorAppSuggestion={konnector}
+          konnectorManifest={get(registryData, 'latest_version.manifest', {})}
+          onClose={() => {
+            setModalDisplayed(false)
+          }}
+          onSilence={() => {
+            setModalDisplayed(false)
+            Alerter.success(t('connector.silenced', { name: name }))
+          }}
         />
       )}
-      <div className="item-icon">
-        <AppIcon alt={t('app.logo.alt', { name })} app={slug} />
+      <div className="item item--ghost" onClick={() => setModalDisplayed(true)}>
+        <div className="item-icon">
+          <AppIcon alt={t('app.logo.alt', { name })} app={slug} />
+        </div>
+        <span className="item-title">{name}</span>
       </div>
-      <span className="item-title">{name}</span>
-    </div>
+    </>
   )
 }
 
 CandidateServiceTile.propTypes = {
   t: PropTypes.func.isRequired,
   client: PropTypes.object.isRequired,
-  app: PropTypes.shape({
+  konnector: PropTypes.shape({
     slug: PropTypes.string.isRequired
   }).isRequired
 }
