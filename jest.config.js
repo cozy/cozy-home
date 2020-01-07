953fafa2ebed3e2b8a88ae5b85d9e2de56c3a251
Yannick Lohse <contact@yannick-lohse.fr>
2020-01-07 13:45:19 +0100

    chore: Removed outdated config line

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
   moduleDirectories: ['src', 'node_modules'],
   moduleNameMapper: {
     '^redux-cozy-client$': '<rootDir>/src/lib/redux-cozy-client',
-    '^cozy-doctypes$': 'cozy-logger/dist/index.js',
     '\\.(png|gif|jpe?g|svg)$': '<rootDir>/test/__mocks__/fileMock.js',
     // identity-obj-proxy module is installed by cozy-scripts
     '.styl$': 'identity-obj-proxy',
