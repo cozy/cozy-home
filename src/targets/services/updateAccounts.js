3e03d0c023de39400241ad0d6a0740e81dc20dea
Cyrille Perois <cperois@outlook.com>
2020-02-03 16:58:39 +0100

    feat: Add deleteAccounts service (#1452)
    * feat: Add deleteAccounts service
    
    This service is executed everytime a cipher is deleted. See
    https://github.com/cozy/cozy-libs/pull/942.
    
    * refactor: Extract polyfillFetch
    
    The same code was in two differents services. This is no longer the case
    as it's extracted in a function used in both places

--- src/targets/services/updateAccounts.js
+++ src/targets/services/updateAccounts.js
@@ -1,22 +1,13 @@
-import fetch from 'node-fetch'
-import FormData from 'form-data'
-import { URL } from 'url'
 import NodeVaultClient from 'cozy-keys-lib/transpiled/NodeVaultClient'
 import CozyClient from 'cozy-client'
 import logger from 'cozy-logger'
+import polyfillFetch from './polyfillFetch'
 
 import updateAccountsPassword from 'cozy-harvest-lib/dist/services/updateAccountsPassword'
 
 const log = logger.namespace('updateAccounts')
 
-if (global) {
-  global.fetch = global.fetch || fetch
-  global.Headers = global.Headers || fetch.Headers
-  global.Response = global.Response || fetch.Response
-  global.Request = global.Request || fetch.Request
-  global.FormData = global.FormData || FormData
-  global.URL = global.URL || URL
-}
+polyfillFetch()
 
 const main = async () => {
   const vaultClient = new NodeVaultClient(process.env.COZY_URL)
