5e27def45fc0b7a0b348885214d285ca091fc796
Yannick Lohse <contact@yannick-lohse.fr>
2020-02-25 15:08:53 +0100

    refactor: Use i18n

--- src/components/HeroHeader/LogoutButton.jsx
+++ src/components/HeroHeader/LogoutButton.jsx
@@ -1,11 +1,11 @@
 import React, { useCallback } from 'react'
-import PropTypes from 'prop-types'
-import { translate } from 'cozy-ui/react/I18n'
+import { useI18n } from 'cozy-ui/react/I18n'
 import { withClient } from 'cozy-client'
 
 import CornerButton from './CornerButton'
 
-const LogoutButton = ({ t, client }) => {
+const LogoutButton = ({ client }) => {
+  const { t } = useI18n()
   const logout = useCallback(async () => {
     await client.logout()
     window.location.reload()
@@ -13,9 +13,4 @@ const LogoutButton = ({ t, client }) => {
   return <CornerButton label={t('app.logout')} icon="logout" onClick={logout} />
 }
 
-LogoutButton.propTypes = {
-  t: PropTypes.func.isRequired,
-  client: PropTypes.object.isRequired
-}
-
-export default translate()(withClient(LogoutButton))
+export default withClient(LogoutButton)
--- src/components/HeroHeader/SettingsButton.jsx
+++ src/components/HeroHeader/SettingsButton.jsx
@@ -1,39 +1,37 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Query, Q, models } from 'cozy-client'
 import AppLinker from 'cozy-ui/react/AppLinker'
-import { translate } from 'cozy-ui/react/I18n'
+import { useI18n } from 'cozy-ui/react/I18n'
 import get from 'lodash/get'
 
 const { applications } = models
 
 import CornerButton from './CornerButton'
 
-const SettingsButton = ({ lang }) => (
-  <Query query={() => Q('io.cozy.apps').where({ slug: 'settings' })}>
-    {({ data }) => {
-      const app = get(data, '[0]')
-      const appHref = get(app, 'links.related')
-      const slug = get(app, 'slug')
-      const displayName = applications.getAppDisplayName(app, lang)
-      return slug && appHref ? (
-        <AppLinker slug={slug} href={appHref}>
-          {({ onClick, href }) => (
-            <CornerButton
-              label={displayName}
-              href={href}
-              onClick={onClick}
-              icon="gear"
-            />
-          )}
-        </AppLinker>
-      ) : null
-    }}
-  </Query>
-)
-
-SettingsButton.propTypes = {
-  t: PropTypes.func.isRequired
+const SettingsButton = () => {
+  const { lang } = useI18n()
+  return (
+    <Query query={() => Q('io.cozy.apps').where({ slug: 'settings' })}>
+      {({ data }) => {
+        const app = get(data, '[0]')
+        const appHref = get(app, 'links.related')
+        const slug = get(app, 'slug')
+        const displayName = applications.getAppDisplayName(app, lang)
+        return slug && appHref ? (
+          <AppLinker slug={slug} href={appHref}>
+            {({ onClick, href }) => (
+              <CornerButton
+                label={displayName}
+                href={href}
+                onClick={onClick}
+                icon="gear"
+              />
+            )}
+          </AppLinker>
+        ) : null
+      }}
+    </Query>
+  )
 }
 
-export default translate()(SettingsButton)
+export default SettingsButton
