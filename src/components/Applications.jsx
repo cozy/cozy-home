f4e33a3b5d187c000d618011d23bb962a1382ded
Yannick Lohse <contact@yannick-lohse.fr>
2020-02-27 13:37:05 +0100

    refactor: Class to function component

--- src/components/Applications.jsx
+++ src/components/Applications.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
 import { connect } from 'react-redux'
 
 import { Query, Q } from 'cozy-client'
@@ -11,58 +11,54 @@ import LoadingPlaceholder from 'components/LoadingPlaceholder'
 import homeConfig from 'config/collect'
 import { receiveApps } from 'ducks/apps'
 
-class LoadingAppTiles extends PureComponent {
-  render() {
-    const { num } = this.props
-    const tiles = []
-    for (let i = 0; i < num; i++) {
-      tiles.push(
-        <div className="item-wrapper" key={i}>
-          <header className="item-header">
-            <div className="item-icon">
-              <LoadingPlaceholder />
-            </div>
-          </header>
-          <h3 className="item-title">
+const LoadingAppTiles = memo(({ num }) => {
+  const tiles = []
+  for (let i = 0; i < num; i++) {
+    tiles.push(
+      <div className="item-wrapper" key={i}>
+        <header className="item-header">
+          <div className="item-icon">
             <LoadingPlaceholder />
-          </h3>
-        </div>
-      )
-    }
-    return <>{tiles}</>
-  }
-}
-
-export class Applications extends PureComponent {
-  render() {
-    const { receiveApps } = this.props
-    const showLogout = !!flag('home.mainlist.show-logout')
-    return (
-      <div className="app-list">
-        <Query query={() => Q('io.cozy.apps')}>
-          {({ data, fetchStatus }) => {
-            if (fetchStatus === 'loaded') {
-              receiveApps(data)
-            }
-            return fetchStatus !== 'loaded' ? (
-              <LoadingAppTiles num="3" />
-            ) : (
-              data
-                .filter(
-                  app =>
-                    app.state !== 'hidden' &&
-                    !homeConfig.filteredApps.includes(app.slug) &&
-                    !flag(`home_hidden_apps.${app.slug.toLowerCase()}`) // can be set in the context with `home_hidden_apps: - drive - banks`for example
-                )
-                .map((app, index) => <AppTile key={index} app={app} />)
-            )
-          }}
-        </Query>
-        {showLogout && <LogoutTile />}
+          </div>
+        </header>
+        <h3 className="item-title">
+          <LoadingPlaceholder />
+        </h3>
       </div>
     )
   }
-}
+  return <>{tiles}</>
+})
+LoadingAppTiles.displayName = LoadingAppTiles
+
+const Applications = memo(({ receiveApps }) => {
+  const showLogout = !!flag('home.mainlist.show-logout')
+  return (
+    <div className="app-list">
+      <Query query={() => Q('io.cozy.apps')}>
+        {({ data, fetchStatus }) => {
+          if (fetchStatus === 'loaded') {
+            receiveApps(data)
+          }
+          return fetchStatus !== 'loaded' ? (
+            <LoadingAppTiles num="3" />
+          ) : (
+            data
+              .filter(
+                app =>
+                  app.state !== 'hidden' &&
+                  !homeConfig.filteredApps.includes(app.slug) &&
+                  !flag(`home_hidden_apps.${app.slug.toLowerCase()}`) // can be set in the context with `home_hidden_apps: - drive - banks`for example
+              )
+              .map((app, index) => <AppTile key={index} app={app} />)
+          )
+        }}
+      </Query>
+      {showLogout && <LogoutTile />}
+    </div>
+  )
+})
+Applications.displayName = Applications
 
 const mapDispatchToProps = dispatch => {
   return {
