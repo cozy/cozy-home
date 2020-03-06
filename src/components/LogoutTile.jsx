daaa54cd004683aec1c7a9f2726f73b8bea24740
Yannick Lohse <contact@yannick-lohse.fr>
2020-03-06 16:30:24 +0100

    fix: Logout icon adapts to screen size

--- src/components/LogoutTile.jsx
+++ src/components/LogoutTile.jsx
@@ -2,6 +2,7 @@ import React, { useCallback } from 'react'
 import Icon from 'cozy-ui/react/Icon'
 import { useI18n } from 'cozy-ui/react/I18n'
 import { useClient } from 'cozy-client'
+import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 
 const LogoutTile = () => {
   const { t } = useI18n()
@@ -14,10 +15,12 @@ const LogoutTile = () => {
     },
     [client]
   )
+  const { isMobile } = useBreakpoints()
+
   return (
     <button onClick={logout} className="item">
       <div className="item-icon">
-        <Icon icon="logout-large" size={40} />
+        <Icon icon="logout-large" size={isMobile ? 32 : 40} />
       </div>
       <h3 className="item-title">{t('logout')}</h3>
     </button>
