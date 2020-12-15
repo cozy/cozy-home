195fac7495075ef93cb557630d2996a0b342552c
Patrick Browne <pt.browne@gmail.com>
2020-12-15 10:18:49 +0100

    test: Add BreakpointsProvider in AppLike and render en locales

--- test/AppLike.jsx
+++ test/AppLike.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { createStore } from 'redux'
 import { Provider as ReduxProvider } from 'react-redux'
 import PropTypes from 'prop-types'
+import { BreakpointsProvider } from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import I18n from 'cozy-ui/react/I18n'
+import enLocale from '../src/locales/en.json'
 
 const fakeDefaultReduxState = {
   apps: [{ slug: 'drive', links: { related: '' } }]
@@ -21,15 +23,14 @@ class AppLike extends React.Component {
   }
 
   render() {
-    const polyglot = {
-      t: s => s
-    }
     return (
-      <ReduxProvider store={this.props.store}>
-        <I18n dictRequire={() => ({})} polyglot={polyglot} lang="en">
-          {this.props.children}
-        </I18n>
-      </ReduxProvider>
+      <BreakpointsProvider>
+        <ReduxProvider store={this.props.store}>
+          <I18n dictRequire={() => enLocale} lang="en">
+            {this.props.children}
+          </I18n>
+        </ReduxProvider>
+      </BreakpointsProvider>
     )
   }
 }
