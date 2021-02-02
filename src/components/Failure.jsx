4ce604de9b7c5b102ad5dd900419b4cadf7fca6a
Patrick Browne <pt.browne@gmail.com>
2021-02-02 18:09:27 +0100

    feat: Update failure to use Typography components

--- src/components/Failure.jsx
+++ src/components/Failure.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 import Stack from 'cozy-ui/transpiled/react/Stack'
 import Button from 'cozy-ui/transpiled/react/Button'
 import Icon from 'cozy-ui/transpiled/react/Icon'
-import { MainTitle } from 'cozy-ui/transpiled/react/Text'
 import EmptyIcon from 'assets/icons/color/default.svg'
-
 import { useI18n } from 'cozy-ui/transpiled/react'
+
+import Typography from 'cozy-ui/transpiled/react/Typography'
 
 const reload = () => {
   window.location.reload()
@@ -18,9 +18,9 @@ export const Failure = ({ errorType }) => {
   return (
     <Stack className="u-flex u-flex-column u-flex-items-center">
       <Icon icon={EmptyIcon} size={64} />
-      <MainTitle tag="h2" className="u-ta-center">
+      <Typography tag="h2" className="u-ta-center" variant="h3" component="h1">
         {t(`error.${errorType}`)}
-      </MainTitle>
+      </Typography>
       <Button label={t('error.button.reload')} onClick={reload} />
     </Stack>
   )
