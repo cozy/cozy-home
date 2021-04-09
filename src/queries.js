a7cc0262a708a868e3cb55562b50f90ce65c14ce
Patrick Browne <pt.browne@gmail.com>
2021-04-09 10:45:44 +0200

    feat: Use a named query for konnector suggestions

--- src/queries.js
+++ src/queries.js
@@ -1,7 +1,18 @@
 import CozyClient, { Q } from 'cozy-client'
 
+const defaultFetchPolicy = CozyClient.fetchPolicies.olderThan(30 * 1000)
 export const appsConn = {
   query: Q('io.cozy.apps'),
   as: 'apps',
-  fetchPolicy: CozyClient.fetchPolicies.olderThan(30 * 1000)
+  fetchPolicy: defaultFetchPolicy
+}
+
+export const suggestedKonnectorsConn = {
+  query: () =>
+    Q('io.cozy.apps.suggestions')
+      .where({ silenced: false })
+      .sortBy([{ silenced: 'asc' }, { slug: 'asc' }])
+      .indexFields(['silenced', 'slug']),
+  as: 'app-suggestions',
+  fetchPolicy: defaultFetchPolicy
 }
