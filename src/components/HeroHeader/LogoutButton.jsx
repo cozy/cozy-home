ee4a8070cb8e990c488402eccc78bb7814861067
Antonin Cezard <acezard@gmail.com>
2021-12-30 09:03:43 +0100

    feat: Use webview intent to logout

--- src/components/HeroHeader/LogoutButton.jsx
+++ src/components/HeroHeader/LogoutButton.jsx
@@ -1,20 +1,24 @@
 import React, { useCallback } from 'react'
+
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import { useClient } from 'cozy-client'
 import LogoutIcon from 'cozy-ui/transpiled/react/Icons/Logout'
+import { useWebviewIntent } from 'cozy-webview'
+import { isFlagshipApp } from 'cozy-device-helper'
 
 import CornerButton from './CornerButton'
 
 const LogoutButton = () => {
   const { t } = useI18n()
   const client = useClient()
+  const webviewIntent = useWebviewIntent()
+
   const logout = useCallback(async () => {
-    if (window.cozy.isWebview)
-      return window.ReactNativeWebView.postMessage('LOGOUT')
+    if (isFlagshipApp()) return webviewIntent.call('logout')
 
     await client.logout()
     window.location.reload()
-  }, [client])
+  }, [client, webviewIntent])
   return <CornerButton label={t('logout')} icon={LogoutIcon} onClick={logout} />
 }
 
