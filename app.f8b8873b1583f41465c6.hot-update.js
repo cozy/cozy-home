b4f062023ad8f6d510ffcea88998c31e66e00c53
Cozy Pierre <pierre.trolle@cozycloud.cc>
2022-04-26 17:39:58 +0200

    publish: fix: Upgrade cozy-intent to latest
    generated from commit 9e2fce3933bfd1245f4ffd2894e47d63f1ebae06

---
+++ app.f8b8873b1583f41465c6.hot-update.js
@@ -0,0 +1,292 @@
+webpackHotUpdate("app",{
+
+/***/ "./node_modules/cozy-client/dist/models/document/locales sync recursive ^\\.\\/.*\\.json$":
+false,
+
+/***/ "./node_modules/cozy-harvest-lib/dist/locales sync recursive ^\\.\\/.*\\.json$":
+false,
+
+/***/ "./node_modules/cozy-ui/node_modules/date-fns/locale sync recursive \\/(en|fr|es)\\/index.js":
+false,
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./node_modules/cozy-scripts/node_modules/stylus-loader/index.js?!./src/styles/index.styl":
+false,
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./node_modules/cozy-scripts/node_modules/stylus-loader/index.js?!./src/styles/placeholder.styl":
+false,
+
+/***/ "./node_modules/moment/locale sync recursive \\/(en|fr|es)\\/index.js":
+false,
+
+/***/ "./src/assets/icons/color/default.svg":
+false,
+
+/***/ "./src/assets/images/drawing-arrow-up.svg":
+false,
+
+/***/ "./src/assets/images/moved-cozy.svg":
+false,
+
+/***/ "./src/components/AddServiceTile.jsx":
+false,
+
+/***/ "./src/components/AppTile.jsx":
+false,
+
+/***/ "./src/components/AppWrapper.jsx":
+false,
+
+/***/ "./src/components/Applications.jsx":
+false,
+
+/***/ "./src/components/CandidateCategoryTile.jsx":
+false,
+
+/***/ "./src/components/CandidateServiceTile.jsx":
+false,
+
+/***/ "./src/components/EmptyServicesListTip.jsx":
+false,
+
+/***/ "./src/components/Failure.jsx":
+false,
+
+/***/ "./src/components/FallbackCandidateServiceTile.jsx":
+false,
+
+/***/ "./src/components/FooterLogo.jsx":
+false,
+
+/***/ "./src/components/HeroHeader/Corner.jsx":
+false,
+
+/***/ "./src/components/HeroHeader/CornerButton.jsx":
+false,
+
+/***/ "./src/components/HeroHeader/HelpButton.jsx":
+false,
+
+/***/ "./src/components/HeroHeader/LogoutButton.jsx":
+false,
+
+/***/ "./src/components/HeroHeader/SettingsButton.jsx":
+false,
+
+/***/ "./src/components/HeroHeader/index.jsx":
+false,
+
+/***/ "./src/components/Home.jsx":
+false,
+
+/***/ "./src/components/IntentRedirect.jsx":
+false,
+
+/***/ "./src/components/Konnector.jsx":
+false,
+
+/***/ "./src/components/KonnectorErrors.jsx":
+false,
+
+/***/ "./src/components/KonnectorTile.jsx":
+false,
+
+/***/ "./src/components/LoadingPlaceholder.jsx":
+false,
+
+/***/ "./src/components/LogoutTile.jsx":
+false,
+
+/***/ "./src/components/MainView.jsx":
+false,
+
+/***/ "./src/components/MoveModal.jsx":
+false,
+
+/***/ "./src/components/ReactMarkdownWrapper.jsx":
+false,
+
+/***/ "./src/components/ScrollToTopOnMount.jsx":
+false,
+
+/***/ "./src/components/Services.jsx":
+false,
+
+/***/ "./src/components/ShortcutLink.jsx":
+false,
+
+/***/ "./src/components/Shortcuts/ShortcutsView.jsx":
+false,
+
+/***/ "./src/components/Shortcuts/index.jsx":
+false,
+
+/***/ "./src/components/Shortcuts/useCustomShortcuts.js":
+false,
+
+/***/ "./src/components/Shortcuts/utils.js":
+false,
+
+/***/ "./src/components/StoreRedirection.jsx":
+false,
+
+/***/ "./src/components/appEntryPoint.jsx":
+false,
+
+/***/ "./src/config/candidates.json":
+false,
+
+/***/ "./src/config/categories.json":
+false,
+
+/***/ "./src/config/home.json":
+false,
+
+/***/ "./src/containers/App.jsx":
+false,
+
+/***/ "./src/containers/toFlagNames.js":
+false,
+
+/***/ "./src/ducks/accounts/index.js":
+false,
+
+/***/ "./src/ducks/apps/index.js":
+false,
+
+/***/ "./src/ducks/connections/index.js":
+false,
+
+/***/ "./src/ducks/jobs/index.js":
+false,
+
+/***/ "./src/ducks/konnectors/index.js":
+false,
+
+/***/ "./src/ducks/triggers/index.js":
+false,
+
+/***/ "./src/flags.js":
+false,
+
+/***/ "./src/hoc/withCustomWallpaper.jsx":
+false,
+
+/***/ "./src/hooks/useCustomWallpaper.jsx":
+false,
+
+/***/ "./src/hooks/useHomeShortcuts.js":
+false,
+
+/***/ "./src/hooks/useInstanceSettings.jsx":
+false,
+
+/***/ "./src/hooks/useRegistryInformation.jsx":
+false,
+
+/***/ "./src/lib/HomeStore.js":
+false,
+
+/***/ "./src/lib/PiwikHashRouter.js":
+false,
+
+/***/ "./src/lib/konnectors.js":
+false,
+
+/***/ "./src/lib/middlewares/konnectorsI18n.js":
+false,
+
+/***/ "./src/lib/redux-cozy-client/CozyClient.js":
+false,
+
+/***/ "./src/lib/redux-cozy-client/CozyProvider.jsx":
+false,
+
+/***/ "./src/lib/redux-cozy-client/adapters/CozyStackAdapter.js":
+false,
+
+/***/ "./src/lib/redux-cozy-client/connect.jsx":
+false,
+
+/***/ "./src/lib/redux-cozy-client/index.js":
+false,
+
+/***/ "./src/lib/redux-cozy-client/middleware.js":
+false,
+
+/***/ "./src/lib/redux-cozy-client/reducer.js":
+false,
+
+/***/ "./src/lib/redux-cozy-client/utils.js":
+false,
+
+/***/ "./src/lib/triggers.js":
+false,
+
+/***/ "./src/locales sync recursive \\/(en|fr|es).json":
+false,
+
+/***/ "./src/locales/en.json":
+false,
+
+/***/ "./src/locales/es.json":
+false,
+
+/***/ "./src/locales/fr.json":
+false,
+
+/***/ "./src/queries.js":
+false,
+
+/***/ "./src/reducers/index.js":
+false,
+
+/***/ "./src/schema.js":
+false,
+
+/***/ "./src/store/configureStore.js":
+false,
+
+/***/ "./src/styles/index.styl":
+false,
+
+/***/ "./src/styles/placeholder.styl":
+false,
+
+/***/ "./src/targets/browser/index.jsx":
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/cozy-scripts/node_modules/eslint-loader/dist/cjs.js):\\nError: ENOENT: no such file or directory, open '/Users/recontact/projects/cozy/cozy-home/src/targets/browser/index.jsx'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvdGFyZ2V0cy9icm93c2VyL2luZGV4LmpzeC5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/targets/browser/index.jsx\n");
+
+/***/ }),
+
+/***/ 10:
+false,
+
+/***/ 11:
+false,
+
+/***/ 12:
+false,
+
+/***/ 3:
+false,
+
+/***/ 4:
+false,
+
+/***/ 5:
+false,
+
+/***/ 6:
+false,
+
+/***/ 7:
+false,
+
+/***/ 8:
+false,
+
+/***/ 9:
+false
+
+})
