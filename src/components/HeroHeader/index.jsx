8d6820c2f955e68951d6a63f2c7d494b08c76c72
Ldoppea <yannick.chiron@gmail.com>
2023-05-10 20:06:34 +0200

    fix: Make the Avatar compatible with normal and inverted Cozy Themes
    On normal theme, the Avatar is now displayed on a light background
    
    As it has a white border, we want it to have a surrounding shadow so we
    have a visible separation between the avatar and the light background
    
    This commit also remove the old `textShadowHeavy` based shadow that did
    not work as `textShadowHeavy` was not existing anywhere

--- src/components/HeroHeader/index.jsx
+++ src/components/HeroHeader/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { useClient } from 'cozy-client'
 import { useInstanceSettings } from 'hooks/useInstanceSettings'
+import cx from 'classnames'
 
+import { makeStyles } from 'cozy-ui/transpiled/react/styles'
 import Typography from 'cozy-ui/transpiled/react/Typography'
+
+const useStyles = makeStyles(theme => ({
+  shadow: {
+    boxShadow: theme.shadows[2]
+  }
+}))
 
 export const HeroHeader = () => {
   const client = useClient()
+  const classes = useStyles()
   const rootURL = client.getStackClient().uri
   const { host } = new URL(rootURL)
 
@@ -16,7 +25,7 @@ export const HeroHeader = () => {
     <header className="hero-header u-pos-relative u-flex u-flex-column u-flex-justify-center u-flex-items-center u-flex-shrink-0 u-bxz">
       <div>
         <img
-          className="hero-avatar u-mb-1 u-mb-half-s"
+          className={cx('hero-avatar u-mb-1 u-mb-half-s', classes.shadow)}
           aria-hidden="true"
           src={`${rootURL}/public/avatar`}
         />
