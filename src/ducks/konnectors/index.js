cca7087be449003e6647d7285bbff83930b9ac96
Crash-- <quentin.valmori@gmail.com>
2023-08-21 11:05:07 +0200

    refactor: Rely on realtime instead of redux action

--- src/ducks/konnectors/index.js
+++ src/ducks/konnectors/index.js
@@ -2,23 +2,6 @@ import keyBy from 'lodash/keyBy'
 
 export const DOCTYPE = 'io.cozy.konnectors'
 
-// Action creators
-export const receiveInstalledKonnector = konnector => {
-  const normalized = {
-    ...konnector,
-    ...konnector.attributes,
-    id: `${DOCTYPE}/${konnector.slug}`,
-    _type: DOCTYPE
-  }
-
-  return {
-    doctype: DOCTYPE,
-    type: 'RECEIVE_NEW_DOCUMENT',
-    response: { data: [normalized] },
-    updateCollections: ['konnectors']
-  }
-}
-
 const getKonnectorsFromState = state => {
   return !!state && !!state.documents && state.documents[DOCTYPE]
 }
@@ -32,11 +15,6 @@ export const getKonnector = (state, slug) => {
 export const getInstalledKonnectors = state => {
   const konnectors = getKonnectorsFromState(state)
   return konnectors ? Object.values(konnectors) : []
-}
-
-export const getIndexedKonnectors = state => {
-  const konnectors = getKonnectorsFromState(state)
-  return konnectors ? keyBy(Object.values(konnectors), konn => konn.slug) : {}
 }
 
 export const getSlugs = state => {
