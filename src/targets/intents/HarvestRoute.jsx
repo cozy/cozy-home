ff2c3f7ff3c037579355c118952ca9e60da50664
Crash-- <quentin.valmori@gmail.com>
2023-08-28 14:59:08 +0200

    fix: Intent can be dismissed
    This intent should be called with closable=false since Harvest will
    handle by itself how to cancel.

--- src/targets/intents/HarvestRoute.jsx
+++ src/targets/intents/HarvestRoute.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Routes } from 'cozy-harvest-lib'
 import { useParams } from 'react-router-dom'
 import CozyTheme from 'cozy-ui/transpiled/react/CozyTheme'
+import { useClient } from 'cozy-client'
+import Intents from 'cozy-interapp'
 
-export const HarvestRoutes = props => {
+export const HarvestRoutes = () => {
   const { konnectorSlug } = useParams()
+  const client = useClient()
+
+  const intents = new Intents({ client })
+  const [service, setService] = useState(null)
+  useEffect(() => {
+    // eslint-disable-next-line
+    intents.createService().then(service => {
+      setService(service)
+    })
+  }, [])
 
   return (
     <CozyTheme variant="normal">
       <Routes
         konnectorRoot={`/${konnectorSlug}`}
         konnectorSlug={konnectorSlug}
+        onDismiss={() => (service ? service.cancel() : undefined)}
       />
     </CozyTheme>
   )
