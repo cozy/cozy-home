38e3529ba2f55082f48f077634e0e38e42983d15
cballevre <celestin.ballevre@protonmail.com>
2023-09-21 16:26:50 +0200

    feat: Display a main logo with secondaries

--- src/components/FooterLogo/FooterLogo.jsx
+++ src/components/FooterLogo/FooterLogo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useClient, useQuery } from 'cozy-client'
 import { buildContextQuery } from 'queries'
+import Divider from 'cozy-ui/transpiled/react/Divider'
 
 import { getHomeLogos } from 'components/FooterLogo/helpers'
 
@@ -13,19 +14,48 @@ export const FooterLogo = () => {
 
   const logos = getHomeLogos(data, rootURL)
 
-  if (Object.keys(logos).length === 0) return null
+  const hasSecondaries =
+    logos.secondaries && Object.keys(logos.secondaries).length !== 0
+  const hasMain = logos.main !== undefined
+
+  if (!hasMain && !hasSecondaries) return null
 
   return (
-    <div className="u-maw-7 u-mh-auto u-mt-1 u-pv-1 u-flex u-flex-row u-flex-items-center u-flex-justify-center u-flex-wrap">
-      {Object.entries(logos).map(([logoSrc, logoAlt]) => (
-        <img
-          key={logoSrc}
-          src={logoSrc}
-          alt={logoAlt}
-          className="u-ph-1 u-pv-half u-mah-5"
-        />
-      ))}
-    </div>
+    <footer className="u-mt-auto">
+      <Divider />
+      <div className="u-flex">
+        <div className="u-flex u-mh-auto u-maw-100 u-flex-items-center">
+          {hasMain ? (
+            <img
+              key={logos.main.url}
+              src={logos.main.url}
+              alt={logos.main.alt}
+              className="u-ph-1 u-pv-1 u-mah-3"
+            />
+          ) : null}
+          {hasMain && hasSecondaries ? (
+            <div style={{ height: '50%' }}>
+              <Divider orientation="vertical" />
+            </div>
+          ) : null}
+          {hasSecondaries ? (
+            <div className="u-flex u-flex-grow-1 u-ov-auto u-filter-gray-100 u-pv-1">
+              {Object.entries(logos.secondaries).map(([logoSrc, logoAlt]) => (
+                <img
+                  key={logoSrc}
+                  src={logoSrc}
+                  alt={logoAlt}
+                  className="u-ph-1 u-mah-3"
+                  style={{
+                    objectFit: 'contain'
+                  }}
+                />
+              ))}
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/components/FooterLogo/helpers.js
+++ src/components/FooterLogo/helpers.js
@@ -2,7 +2,17 @@ export const getHomeLogos = (data, rootURL) => {
   if (data) {
     const logos = data?.attributes?.home_logos || {}
     return Object.keys(logos).reduce((acc, logoSrc) => {
-      acc[`${rootURL}/assets${logoSrc}`] = logos[logoSrc]
+      const filename = logoSrc.substring(logoSrc.lastIndexOf('/') + 1)
+      const logo = {
+        url: `${rootURL}/assets${logoSrc}`,
+        alt: logos[logoSrc]
+      }
+      if (filename.startsWith('main_')) {
+        acc['main'] = logo
+      } else {
+        acc['secondaries'] = acc['secondaries'] || {}
+        acc['secondaries'][logo.url] = logo.alt
+      }
       return acc
     }, {})
   }
