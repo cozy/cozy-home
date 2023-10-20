cbe06b524ee02417085de84b8284e993dc1c69cc
Théo Poizat <hello@zatteo.com>
2023-10-20 12:30:05 +0200

    refactor: Add logic to manage multiple AppHighlightAlert
    Multiple AppHighlightAlert will be available soon. We need to be able
    to :
    - check the available ones => flag enabled for backup example
    - check the displayable ones => at second time we open the home for
    backup example
    - select one to display when there are multiple displayables ones
    - do not display anymore when an AppHighlightAlert has been shown

---
+++ src/components/AppHighlightAlert/AppHighlightAlertWrapper.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+
+import { getBackupAppHighlightAlert } from 'components/AppHighlightAlert/BackupAppHighlightAlert'
+
+const AppHighlightAlertWrapper = ({ apps }) => {
+  const [appHighlightAlerts, setAppHighlightAlerts] = useState([])
+
+  useEffect(() => {
+    const appHighlightAlerts = [getBackupAppHighlightAlert()]
+
+    const availableAppHighlightAlerts = appHighlightAlerts.filter(
+      status => status.available
+    )
+
+    const selectedIndex = availableAppHighlightAlerts.findIndex(
+      status => status.displayable
+    )
+    if (selectedIndex !== -1) {
+      availableAppHighlightAlerts[selectedIndex].displayed = true
+    }
+
+    setAppHighlightAlerts(availableAppHighlightAlerts)
+  }, [])
+
+  useEffect(() => {
+    appHighlightAlerts.forEach(component => {
+      if (component.displayed) {
+        component.onDisplayed()
+      } else {
+        component.onNotDisplayed()
+      }
+    })
+  }, [appHighlightAlerts])
+
+  return (
+    <>
+      {appHighlightAlerts.map(({ Component, name, displayed }) => (
+        <Component key={name} apps={apps} displayed={displayed} />
+      ))}
+    </>
+  )
+}
+
+export default AppHighlightAlertWrapper
