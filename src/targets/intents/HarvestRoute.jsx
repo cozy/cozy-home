7a647c70e2ece134b2c85dd1eec1fa5456c7b034
Crash-- <quentin.valmori@gmail.com>
2023-10-24 13:37:50 +0200

    fix: Lint

--- src/targets/intents/HarvestRoute.jsx
+++ src/targets/intents/HarvestRoute.jsx
@@ -17,6 +17,7 @@ export const HarvestRoutes = () => {
     intents.createService().then(service => {
       setService(service)
     })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
