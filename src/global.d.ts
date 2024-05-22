92fe08bac809adb1d661bbace65da59e284f5eeb
Antonin Cezard <acezard@gmail.com>
2024-05-22 12:02:29 +0200

    fix: global type issue fixed

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,8 @@
 declare module 'cozy-harvest-lib'
 declare module 'cozy-ui/transpiled/react/providers/Breakpoints' {
+  export function BreakpointsProvider(props: {
+    children: React.ReactNode
+  }): JSX.Element
   export default function useBreakpoints(): { isMobile: boolean }
 }
 declare module 'cozy-ui/transpiled/react/Spinner'
