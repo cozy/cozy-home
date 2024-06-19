1926b6bc3a28225c6252ca50f894f75a2a5cdbbf
Antonin Cezard <acezard@gmail.com>
2024-06-19 15:48:29 +0200

    fix: style/locale issues
    remain router issue

--- src/components/Sections/queries/konnectors.ts
+++ src/components/Sections/queries/konnectors.ts
@@ -45,7 +45,7 @@ export const fetchAllKonnectors = async (
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
   const { data } = (await client.stackClient.fetchJSON(
     'GET',
-    `/registry?versionsChannel=${channel}&filter[type]=konnector`
+    `/registry?versionsChannel=${channel}&filter[type]=konnector&limit=300`
   )) as { data: Konnector[] }
 
   return memoizedGroupByCategory(data)
