f3d9d6dcf56b3168710e918dd3fb5b7349183fdd
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-11-04 13:54:43 +0100

    feat(Assistant): Add possiblity to navigate with arrow keys in SearchResult

--- src/assistant/Search/SearchBarDesktop.jsx
+++ src/assistant/Search/SearchBarDesktop.jsx
@@ -18,10 +18,11 @@ import styles from './styles.styl'
 
 const SearchBarDesktop = ({ value, onClear, onChange }) => {
   const { t } = useI18n()
-  const { searchValue } = useSearch()
+  const { searchValue, results, selectedIndex, setSelectedIndex } = useSearch()
   const { onAssistantExecute } = useAssistant()
   const navigate = useNavigate()
   const searchRef = useRef()
+  const listRef = useRef()
 
   const handleClick = () => {
     if (!flag('cozy.assistant.enabled')) return
@@ -37,9 +38,41 @@ const SearchBarDesktop = ({ value, onClear, onChange }) => {
   }
 
   const handleKeyDown = ev => {
+    const listElementCount = listRef.current?.childElementCount
+
+    if (ev.key === 'ArrowDown') {
+      ev.preventDefault()
+
+      if (selectedIndex === listElementCount - 1) {
+        setSelectedIndex(0)
+      } else {
+        setSelectedIndex(v => v + 1)
+      }
+    }
+
+    if (ev.key === 'ArrowUp') {
+      ev.preventDefault()
+
+      if (selectedIndex === 0) {
+        setSelectedIndex(listElementCount - 1)
+      } else {
+        setSelectedIndex(v => v - 1)
+      }
+    }
+
+    if (ev.key === 'Escape') {
+      ev.preventDefault()
+      onClear()
+    }
+
     if (ev.key === 'Enter') {
-      ev.preventDefault() // prevent form submit
-      if (value !== '') handleClick()
+      ev.preventDefault()
+      if (selectedIndex) {
+        const onClickFn = results?.[selectedIndex - 1]?.onClick || handleClick
+        onClickFn()
+      } else if (value !== '') {
+        handleClick()
+      }
     }
   }
 
@@ -61,7 +94,11 @@ const SearchBarDesktop = ({ value, onClear, onChange }) => {
           onChange={onChange}
         />
         {searchValue && (
-          <ResultMenu anchorRef={searchRef} onClick={handleClick} />
+          <ResultMenu
+            listRef={listRef}
+            anchorRef={searchRef}
+            onClick={handleClick}
+          />
         )}
       </span>
     </ClickAwayListener>
