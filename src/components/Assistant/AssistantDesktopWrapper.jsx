a71a3617e9da1a39ff0e143068f559c5320095bd
Théo Poizat <hello@zatteo.com>
2025-04-04 11:36:00 +0200

    feat: Update search bar for Twake theme

--- src/components/Assistant/AssistantDesktopWrapper.jsx
+++ src/components/Assistant/AssistantDesktopWrapper.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 import CozyTheme from 'cozy-ui/transpiled/react/providers/CozyTheme'
+import { isTwakeTheme } from 'cozy-ui/transpiled/react/helpers/isTwakeTheme'
 
 import { AssistantDesktop } from 'cozy-search'
+
+import styles from './assistant.styl'
 
 export const AssistantDesktopWrapper = () => {
   return (
@@ -10,10 +13,17 @@ export const AssistantDesktopWrapper = () => {
       <div className="app-list-wrapper u-mh-auto u-mb-3">
         <AssistantDesktop
           componentsProps={{
-            SearchBarDesktop: {
-              elevation: true,
-              hasHalfBorderRadius: true
-            }
+            SearchBarDesktop: isTwakeTheme()
+              ? {
+                  size: 'medium',
+                  className: styles['search-bar-background'],
+                  elevation: 20,
+                  hasHalfBorderRadius: true
+                }
+              : {
+                  elevation: 1,
+                  hasHalfBorderRadius: true
+                }
           }}
         />
       </div>
