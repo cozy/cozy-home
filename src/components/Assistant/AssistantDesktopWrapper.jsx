c4fdb6c005eb43511242fc9875d17022413d8e59
Théo Poizat <hello@zatteo.com>
2025-04-07 15:46:16 +0200

    fix: Correct color for search bar in dark mode

--- src/components/Assistant/AssistantDesktopWrapper.jsx
+++ src/components/Assistant/AssistantDesktopWrapper.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import CozyTheme from 'cozy-ui/transpiled/react/providers/CozyTheme'
+import CozyTheme, {
+  useCozyTheme
+} from 'cozy-ui/transpiled/react/providers/CozyTheme'
 import { isTwakeTheme } from 'cozy-ui/transpiled/react/helpers/isTwakeTheme'
 
 import { AssistantDesktop } from 'cozy-search'
@@ -8,6 +10,8 @@ import { AssistantDesktop } from 'cozy-search'
 import styles from './assistant.styl'
 
 export const AssistantDesktopWrapper = () => {
+  const { type } = useCozyTheme()
+
   return (
     <CozyTheme variant="normal">
       <div className="app-list-wrapper u-mh-auto u-mb-3">
@@ -16,7 +20,7 @@ export const AssistantDesktopWrapper = () => {
             SearchBarDesktop: isTwakeTheme()
               ? {
                   size: 'medium',
-                  className: styles['search-bar-background'],
+                  className: styles[`search-bar-background--${type}`],
                   hasHalfBorderRadius: true
                 }
               : {
