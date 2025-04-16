e80b113a566537400d5184924690af7b9b690a2a
Théo Poizat <hello@zatteo.com>
2025-04-16 08:54:19 +0200

    feat: Add an AssistantTile that open ConversationBar

--- src/components/ApplicationsAndServices.jsx
+++ src/components/ApplicationsAndServices.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 
 import flag from 'cozy-flags'
+import { useBreakpoints } from 'cozy-ui/transpiled/react/providers/Breakpoints'
 
 import LogoutTile from 'components/LogoutTile'
 import ShortcutLink from 'components/ShortcutLink'
 
 import { useApps } from './Applications'
 import { useServices } from './Services'
+import AssistantTile from './AssistantTile'
 import AddTile from './AddTile'
 
 export const ApplicationsAndServices = () => {
   const showLogout = !!flag('home.mainlist.show-logout')
   const { apps, shortcuts } = useApps()
   const { konnectors } = useServices()
+  const { isMobile } = useBreakpoints()
 
   return (
     <div className="app-list-wrapper u-m-auto u-w-100">
@@ -23,6 +26,7 @@ export const ApplicationsAndServices = () => {
           shortcuts.map((shortcut, index) => (
             <ShortcutLink key={index} file={shortcut} />
           ))}
+        {isMobile && <AssistantTile />}
         {<AddTile />}
         {showLogout && <LogoutTile />}
       </div>
