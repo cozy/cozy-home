52d705c35cc7b37b41c89392891d10d3f7ff09ac
Théo Poizat <hello@zatteo.com>
2025-05-27 13:16:47 +0200

    feat: Use same color as desktop search for mobile search

--- src/components/Assistant/AssistantMobileWrapper.jsx
+++ src/components/Assistant/AssistantMobileWrapper.jsx
@@ -7,25 +7,27 @@ import CozyTheme, {
   useCozyTheme
 } from 'cozy-ui/transpiled/react/providers/CozyTheme'
 
-import { useWallpaperContext } from 'hooks/useWallpaperContext'
 import { AssistantMobile } from 'cozy-search'
+
+import styles from './assistant.styl'
 
 export const AssistantMobileWrapper = () => {
   const { type } = useCozyTheme()
-
-  const {
-    data: { isCustomWallpaper }
-  } = useWallpaperContext()
 
   return (
     <CozyTheme variant="normal">
       <div
         className={cx('home-mobile-assistant', {
-          [`home-mobile-assistant--${type}`]: !isCustomWallpaper,
           ['home-mobile-assistant--immersive']: getFlagshipMetadata().immersive
         })}
       >
-        <AssistantMobile />
+        <AssistantMobile
+          componentsProps={{
+            SearchBar: {
+              className: styles[`search-bar-background--${type}`]
+            }
+          }}
+        />
       </div>
     </CozyTheme>
   )
