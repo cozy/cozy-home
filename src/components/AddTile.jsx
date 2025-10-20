c86bbc5f78fae8768bfd693fff8d6b203c254b3b
Théo Poizat <hello@zatteo.com>
2025-10-20 14:55:15 +0200

    refactor: Improve AddTile organization
    AddTile can do two things depending of multiple factors:
    - open shortcut modal
    - open store app
    
    In dcf5018c95d4caf3a14ae14573bf2921bb3492be, we moved down the
    isStoreAvailable check that was previsouly conditioning the
    visibility of AddTile inside AppTile. Without this, if the store was
    not installed, even if we wanted "open shortcut modal" behavior, it
    was not possible because the AppTile component was never mounted.
    
    Here I separate AppTile in two subcomponents and properly check which
    one should be displayed in the correct order in the AppTile which
    become a wrapper.

--- src/components/AddTile.jsx
+++ src/components/AddTile.jsx
@@ -8,56 +8,74 @@ import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 import styles from '@/styles/lists.styl'
 import ShortcutCreateModal from './Shortcuts/ShortcutCreateModal'
+
+const ShortcutAddTile = () => {
+  const { t } = useI18n()
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false)
+
+  return (
+    <>
+      <a
+        onClick={() => setIsCreateModalOpen(true)}
+        className={styles['scale-hover']}
+      >
+        <SquareAppIcon name={t('add_service')} variant="add" />
+      </a>
+      {isCreateModalOpen && (
+        <ShortcutCreateModal onClose={() => setIsCreateModalOpen(false)} />
+      )}
+    </>
+  )
+}
+
+const StoreAddTile = () => {
+  const client = useClient()
+  const { t } = useI18n()
+  const nativePath = '/discover'
+  const slug = 'store'
+  const cozyURL = new URL(client.getStackClient().uri)
+  const { subdomain: subDomainType } = client.getInstanceOptions()
+
+  return (
+    <AppLinker
+      app={{ slug: 'store' }}
+      nativePath={nativePath}
+      href={generateWebLink({
+        pathname: '/',
+        cozyUrl: cozyURL.origin,
+        slug,
+        hash: nativePath,
+        subDomainType
+      })}
+    >
+      {({ onClick, href }) => (
+        <a onClick={onClick} href={href} className={styles['scale-hover']}>
+          <SquareAppIcon name={t('add_service')} variant="add" />
+        </a>
+      )}
+    </AppLinker>
+  )
+}
+
 /**
  * AddTile component.
  *
  * @returns {JSX.Element} The rendered AddTile component.
  */
-const AddTile = ({ isStoreAvailable }) => {
-  const client = useClient()
-  const nativePath = '/discover'
-  const slug = 'store'
-  const cozyURL = new URL(client.getStackClient().uri)
-  const { subdomain: subDomainType } = client.getInstanceOptions()
-  const { t } = useI18n()
-  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false)
+const AddTile = ({ apps }) => {
+  const shouldDisplayShortcutAddTitle = flag('home.add-tile.add-shortcut')
 
-  if (flag('home.add-tile.add-shortcut')) {
-    return (
-      <>
-        <a
-          onClick={() => setIsCreateModalOpen(true)}
-          className={styles['scale-hover']}
-        >
-          <SquareAppIcon name={t('add_service')} variant="add" />
-        </a>
-        {isCreateModalOpen && (
-          <ShortcutCreateModal onClose={() => setIsCreateModalOpen(false)} />
-        )}
-      </>
-    )
+  if (shouldDisplayShortcutAddTitle) {
+    return <ShortcutAddTile />
   }
 
-  if (isStoreAvailable) {
-    return (
-      <AppLinker
-        app={{ slug: 'store' }}
-        nativePath={nativePath}
-        href={generateWebLink({
-          pathname: '/',
-          cozyUrl: cozyURL.origin,
-          slug,
-          hash: nativePath,
-          subDomainType
-        })}
-      >
-        {({ onClick, href }) => (
-          <a onClick={onClick} href={href} className={styles['scale-hover']}>
-            <SquareAppIcon name={t('add_service')} variant="add" />
-          </a>
-        )}
-      </AppLinker>
-    )
+  const hiddenApps = flag('apps.hidden') || []
+  const isStoreInstalled = apps.find(({ slug }) => slug === 'store')
+  const isStoreHidden = hiddenApps.includes('store')
+  const shouldDisplayStoreAddTitle = isStoreInstalled && !isStoreHidden
+
+  if (shouldDisplayStoreAddTitle) {
+    return <StoreAddTile />
   }
 
   return null
